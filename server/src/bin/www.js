29ccceafe78bdeda83c110ed3f4b828e51dd12cb
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-11 18:02:21 +0900

    feat: design db models 1차 db설계

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -1,7 +1,7 @@
 import app from '../app';
 import dbSync from './dbSync';
 
-dbSync({ force: true }).then(() => {
+dbSync({ force: false }).then(() => {
   app.listen(3000, () => {
     console.log('Server start');
   });
