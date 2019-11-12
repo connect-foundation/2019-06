9a94a9a82bbaa2447d1ae41bf6853620fcde2db1
sjh2428 <pok_gare@naver.com>
2019-11-12 12:18:48 +0900

    feat: add stylesheet for fonts and icons

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,5 +1,6 @@
 import App from 'next/app';
 import React from 'react';
+import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 
 const theme = {
@@ -12,9 +13,18 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
     );
   }
 }
