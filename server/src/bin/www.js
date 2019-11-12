3f296672375422ffb0a7c081fff35af54f3eb04c
hwanlee <ljhw3377@gmail.com>
2019-11-12 13:35:55 +0900

    chore: load port environment variable
    포트 번호를 쉽게 설정할 수  있도록 환경변수에서 불러오도록 함

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -3,9 +3,10 @@ import dbSync from './dbSync';
 
 const env = process.env.NODE_ENV || 'development';
 const force = env === 'development';
+const port = process.env.PORT || 3000;
 
 dbSync({ force }).then(() => {
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log('Server start');
   });
 });
