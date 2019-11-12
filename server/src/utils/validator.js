2bfedac555a9c241e0538f61d627523ddf56cfb3
hwanlee <ljhw3377@gmail.com>
2019-11-12 22:51:48 +0900

    refactor: simplify validator module
    validator 모듈을 간소화

--- server/src/utils/validator.js
+++ server/src/utils/validator.js
@@ -1,47 +1,13 @@
-const joinRegex = {
-  id: [
-    {
-      // '아이디는 5~20자의 영문 소문자,숫자와 특수기호(_)(-)만 사용 가능합니다.',
-      regex: /^[a-z0-9-_]{5,20}$/,
-    },
-  ],
-  email: [
-    {
-      // '이메일은 5~50자의 영문 소문자,숫자와 특수기호(_)(-)만 사용 가능합니다.',
-      regex: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-    },
-  ],
-  password: [
-    {
-      // '비밀번호는 8자 이상 20자 이하로 입력해주세요.'
-      regex: /^.{8,20}$/,
-    },
-  ],
-  name: [
-    {
-      // '이름을 정확하게 입력하세요.'
-      regex: /^[가-힣]{1,10}$/,
-    },
-  ],
+const regexOfType = {
+  id: /^[a-z0-9-_]{5,20}$/,
+  email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+  password: /^.{8,20}$/,
+  name: /^[가-힣]{1,10}$/,
 };
 
-const checkLength = (value, min, max) => {
-  return min <= value && value <= max;
-};
-
-const validate = (type, str) => {
-  const list = joinRegex[type];
-
-  for (const { regex, range } of list) {
-    if (regex && !regex.test(str)) {
-      return false;
-    }
-    const [MIN, MAX] = [0, 1];
-    if (range && !checkLength(str.length, range[MIN], range[MAX])) {
-      return false;
-    }
-  }
-
+const validate = (type, value) => {
+  const regex = regexOfType[type];
+  if (!regex.test(value)) return false;
   return true;
 };
 
