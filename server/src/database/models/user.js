8f446e6e5ddd9fe289b80562c47045bf24a4db6b
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-13 15:02:00 +0900

    fix: remove duplicate column in user model

--- server/src/database/models/user.js
+++ server/src/database/models/user.js
@@ -20,11 +20,6 @@ const model = (sequelize, DataTypes) => {
         type: DataTypes.STRING(255),
         allowNull: false,
       },
-      user_id: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        unique: true,
-      },
       password: {
         type: DataTypes.STRING(255),
         allowNull: false,
