6cbf74c9ffbf86779ff18ded8e95ef43af0f9e91
hwanlee <ljhw3377@gmail.com>
2019-11-13 21:39:41 +0900

    refactor: simplify code

--- server/src/v1/users/users.ctrl.js
+++ server/src/v1/users/users.ctrl.js
@@ -4,25 +4,23 @@ import ERROR_CODE from '../../libraries/error-code';
 import ErrorResponse from '../../libraries/error-response';
 
 const registerUser = async (req, res, next) => {
-  try {
-    const { name, id, password, email } = req.body;
-
-    if (!validator.checkUser({ name, email, password, id })) {
-      return next(new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE));
-    }
-
-    const [newUser, isCreated] = await db.User.checkIdAndCreate({ id, name, password, email });
-
-    if (!isCreated) {
-      return next(new ErrorResponse(ERROR_CODE.ID_DUPLICATION));
-    }
-
-    const user = newUser.get({ plain: true });
-
-    res.status(201).json({ user });
-  } catch (error) {
-    next(error);
+  const { name, id, password, email } = req.body;
+  if (!validator.checkUser({ name, email, password, id })) {
+    return next(new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE));
   }
+
+  let newUser, isCreated;
+  try {
+    [newUser, isCreated] = await db.User.checkIdAndCreate({ id, name, password, email });
+  } catch (error) {
+    return next(error);
+  }
+
+  if (!isCreated) {
+    return next(new ErrorResponse(ERROR_CODE.ID_DUPLICATION));
+  }
+  const user = newUser.get({ plain: true });
+  res.status(201).json({ user });
 };
 
 export default { registerUser };
