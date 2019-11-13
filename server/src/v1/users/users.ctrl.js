05ff9c1dbb486dd22389134522a7f735ae7ddbe0
hwanlee <ljhw3377@gmail.com>
2019-11-13 17:02:58 +0900

    refactor: add beforeCreate in user model
    user를 table에 만들기전에 email을 추가하였으며 password를 암호화 하도록 함

--- server/src/v1/users/users.ctrl.js
+++ server/src/v1/users/users.ctrl.js
@@ -1,5 +1,3 @@
-import bcrypt from 'bcrypt';
-
 import db from '../../database';
 import validator from '../../utils/validator';
 
@@ -11,20 +9,12 @@ const registerUser = async (req, res, next) => {
       return res.status(422).end(); // 요청은 잘 만들어졌지만, 문법 오류로 인하여 따를 수 없습니다.
     }
 
-    const { DEFAULT_DOMAIN_NO, DEFAULT_DOMAIN_NAME } = process.env;
-    const userEmail = id + DEFAULT_DOMAIN_NAME;
-    const round = parseInt(process.env.SALT_ROUND, 10);
-    const salt = await bcrypt.genSalt(round);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
     const [newUser, isCreated] = await db.User.findOrCreate({
-      where: { email: userEmail },
+      where: { user_id: id },
       defaults: {
-        domain_no: DEFAULT_DOMAIN_NO,
         user_id: id,
         name,
-        password: hashedPassword,
-        mail: userEmail,
+        password,
         sub_email: email,
       },
     });
