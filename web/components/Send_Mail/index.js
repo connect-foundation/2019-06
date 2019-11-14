1ddbadcc81c81b4f1df65b9940a0a09af11cb440
sjh2428 <pok_gare@naver.com>
2019-11-14 15:57:44 +0900

    feat: add send and reserve button
    보내기와 예약하기 버튼 추가

--- web/components/Send_Mail/index.js
+++ web/components/Send_Mail/index.js
@@ -3,12 +3,14 @@ import S from './styled';
 import InputReceiver from './Input_Receiver';
 import InputSubject from './Input_Subject';
 import InputBody from './Input_Body';
+import SubmitButton from './Submit_Button';
 
 const SendMail = () => (
   <S.DivWrite>
     <InputReceiver />
     <InputSubject />
     <InputBody />
+    <SubmitButton />
   </S.DivWrite>
 );
 
