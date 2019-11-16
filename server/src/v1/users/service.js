c2b7f176e31d4512bfb351d34b7524f6cc849352
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-17 01:17:30 +0900

    feat: implement join service logic 회원가입로직을 웹종속성을 제거한 service부터 구현

---
+++ server/src/v1/users/service.js
@@ -0,0 +1,20 @@
+import DB from '../../database';
+import ErrorResponse from '../../libraries/error-response';
+import ERROR_CODE from '../../libraries/error-code';
+
+// eslint-disable-next-line camelcase
+const register = async ({ user_id, password, name, sub_email }) => {
+  const userData = { user_id, password, name, sub_email };
+
+  const [response, created] = await DB.User.findOrCreateById(userData);
+
+  if (!created) {
+    throw new ErrorResponse(ERROR_CODE.ID_DUPLICATION);
+  }
+
+  const newUser = response.get({ plain: true });
+  delete newUser.password;
+  return newUser;
+};
+
+export default { register };
