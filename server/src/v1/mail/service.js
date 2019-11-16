3ef3c1335dbe35730638335234ed92623516c105
sjh2428 <pok_gare@naver.com>
2019-11-17 00:18:12 +0900

    feat: add service of send mail
    nodemailer를 활용한 이메일 보내기 service 코드 추가

--- server/src/v1/mail/service.js
+++ server/src/v1/mail/service.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+import nodemailer from 'nodemailer';
 import DB from '../../database/index';
 
 const getRawMails = async (userNo, userEmail) => {
@@ -6,4 +7,35 @@ const getRawMails = async (userNo, userEmail) => {
   return mails;
 };
 
-export default { getRawMails };
+const sendMail = async ({ from, to, subject, text, attachments = [] }) => {
+  const { DEFAULT_DOMAIN_NAME, MAIL_AUTH_USER, MAIL_AUTH_PASS } = process.env;
+  const transporter = nodemailer.createTransport({
+    host: DEFAULT_DOMAIN_NAME,
+    port: 465,
+    secure: true,
+    auth: {
+      user: MAIL_AUTH_USER,
+      pass: MAIL_AUTH_PASS,
+    },
+  });
+
+  attachments = attachments.map(attachment => ({
+    // filename, buffer -> content, mimetype -> contentType
+    filename: attachment.originalname,
+    content: attachment.buffer,
+    contentType: attachment.mimetype,
+  }));
+
+  const mailContents = {
+    from,
+    to,
+    subject,
+    text,
+    attachments,
+  };
+
+  await transporter.sendMail(mailContents);
+  return mailContents;
+};
+
+export default { getRawMails, sendMail };
