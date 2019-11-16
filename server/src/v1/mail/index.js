a7be66d17286d8de8282ad121a66d3c823d575bd
sjh2428 <pok_gare@naver.com>
2019-11-16 23:57:41 +0900

    feat: add mail post router with multer
    메일 쓰기와 관련한 라우터 작성
    파일 갯수는 최대 5개

--- server/src/v1/mail/index.js
+++ server/src/v1/mail/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import controller from './controller';
 
 const router = Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get('/', controller.list);
+router.post('/', upload.array('attachments', 5), controller.write);
 
 export default router;
