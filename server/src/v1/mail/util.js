8fb061604eb99d872c51c57b46e5d025e7130a99
sjh2428 <pok_gare@naver.com>
2019-11-17 04:04:22 +0900

    fix: attachment key and transport key in util.js
    attachment의 키와 transport의 키를 수정하였음

--- server/src/v1/mail/util.js
+++ server/src/v1/mail/util.js
@@ -1,7 +1,7 @@
 const getMailData = ({ from, to, subject, text, attachments = [] }) => {
   // filename, buffer -> content, mimetype -> contentType
   attachments = attachments.map(attachment => ({
-    filename: attachment.originalname,
+    name: attachment.originalname,
     content: attachment.buffer,
     contentType: attachment.mimetype,
   }));
@@ -18,7 +18,7 @@ const getMailData = ({ from, to, subject, text, attachments = [] }) => {
 const getTransport = () => {
   const { DEFAULT_DOMAIN_NAME, MAIL_AUTH_USER, MAIL_AUTH_PASS } = process.env;
   return {
-    host: DEFAULT_DOMAIN_NAME,
+    host: `mail.${DEFAULT_DOMAIN_NAME}`,
     port: 465,
     secure: true,
     auth: {
