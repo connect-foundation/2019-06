38e61ce416e610a30d04f0db3d0dc768f5578ed6
sjh2428 <pok_gare@naver.com>
2019-11-17 00:15:43 +0900

    feat: add mail write controller without save to db
    데이터베이스에 저장하는 부분을 제외한 nodemailer를 통하여 전송하는 부분 작성

--- server/src/v1/mail/controller.js
+++ server/src/v1/mail/controller.js
@@ -1,4 +1,6 @@
 import service from './service';
+import ERROR_CODE from '../../libraries/error-code';
+import ErrorResponse from '../../libraries/error-response';
 
 const list = async (req, res, next) => {
   const { no, email } = req.user;
@@ -13,6 +15,21 @@ const list = async (req, res, next) => {
   return res.json({ mails });
 };
 
+const write = async (req, res, next) => {
+  const attachments = req.files;
+  const { from, to, subject, text } = req.body;
+
+  let mail;
+  try {
+    mail = await service.sendMail({ from, to, subject, text, attachments });
+  } catch (error) {
+    return next(new ErrorResponse(ERROR_CODE.FAIL_TO_SEND_MAIL));
+  }
+
+  return res.status(201).json({ mail });
+};
+
 export default {
   list,
+  write,
 };
