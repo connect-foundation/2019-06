e97fec18ec61ff3fc6be4f77b0e537bef1786a98
Jin <dkwk1254@naver.com>
2019-11-16 23:25:16 +0900

    feat: make view to read mail
    임시 데이터에 body를 추가하여 본문 내용이 출력하는지 확인
    메일의 제목, 보낸시간, 보낸 사람, 받은 사람, 본문 출력

--- web/components/ReadMail/index.js
+++ web/components/ReadMail/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import * as S from './styled';
 
 const ReadMail = ({ mail }) => {
-  const { to, from, subject, date } = mail;
+  const { to, from, subject, date, body } = mail;
   return (
-    <>
-      <div>{to}</div>
-      <div>{from}</div>
-      <div>{subject}</div>
-      <div>{date}</div>
-    </>
+    <S.ReadArea>
+      <S.TitleView>
+        <S.Subject>
+          <h4>{subject}</h4>
+          <div>{date}</div>
+        </S.Subject>
+        <S.Address>
+          <span>보낸 사람</span>
+          <div>{from}</div>
+        </S.Address>
+        <S.Address>
+          <span>받은 사람</span>
+          <div>{to}</div>
+        </S.Address>
+      </S.TitleView>
+      <S.ReadFrame>{body}</S.ReadFrame>
+    </S.ReadArea>
   );
 };
 
--- web/components/ReadMail/styled.js
+++ web/components/ReadMail/styled.js
@@ -1,3 +1,36 @@
 import styled from 'styled-components';
 
-export {};
+const ReadArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 8px;
+`;
+
+const TitleView = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 2px solid #e9ecef;
+  padding-bottom: 16px;
+  margin-bottom: 16px;
+`;
+
+const Subject = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Address = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  span {
+    margin: 8px 8px 8px 0;
+    font-weight: 700;
+  }
+`;
+
+const ReadFrame = styled.div``;
+
+export { ReadArea, TitleView, Subject, Address, ReadFrame };
