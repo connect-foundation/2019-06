ca34a2a9a0f34fc89083d337fef53c321cbe99fc
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-16 15:33:40 +0900

    test: add default query test 서버구현에 사용할 기본쿼리 테스트

--- server/test/mail_template/database.spec.js
+++ server/test/mail_template/database.spec.js
@@ -1,14 +1,34 @@
 /* eslint-disable no-undef */
+import should from 'should';
 import DB from '../../src/database';
 
 describe('MailTemplate DB test...', () => {
   before(async () => {
-    DB.sequelize.sync();
+    DB.sequelize.sync({ force: true });
   });
 
   it('create test', async () => {
-    const body = { from: 'daitnu@daitnu.com', to: 'daitne@daitnu.com', title: 'db test', subject: 'subject', tmp: '그냥 만들어봄 ㅎ.ㅎ' };
-    const result = await DB.MailTemplate.create(body);
-    console.log(result);
+    const body = {
+      from: 'daitnu@daitnu.com',
+      to: 'daitne@daitnu.com',
+      title: 'db test',
+      subject: 'subject',
+      tmp: '그냥 만들어봄 ㅎ.ㅎ',
+      body: '바디입니다.',
+    };
+
+    const result = await DB.MailTemplate.create({ ...body });
+    const data = result.get({ plain: true });
+    data.should.be.properties({
+      from: 'daitnu@daitnu.com',
+      to: 'daitne@daitnu.com',
+      subject: 'subject',
+      body: '바디입니다.',
+    });
+  });
+
+  it('select test', async () => {
+    const result = await DB.MailTemplate.findByPk(1, { raw: true });
+    result.should.have.keys('from', 'to', 'subject', 'body');
   });
 });
