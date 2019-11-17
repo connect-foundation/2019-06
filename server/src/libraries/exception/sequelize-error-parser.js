633edda2b120ffd0df8bec54d0b7dc9840da8984
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-17 11:14:32 +0900

    fix: getErrorResponseBySequelizeValidationError map을 사용해서 간결하게 표현

--- server/src/libraries/exception/sequelize-error-parser.js
+++ server/src/libraries/exception/sequelize-error-parser.js
@@ -1,15 +1,13 @@
+/* eslint-disable max-len */
 import ErrorResponse from '../error-response';
 import ERROR_CODE from '../error-code';
 import ErrorField from '../error-field';
 
-const getErrorResponseBySequelizeValidationError = error => {
-  const errorFileds = [];
-  for (const errorFiled of error.errors) {
-    const { message, path, value } = errorFiled;
-    const errorField = new ErrorField(path, value, message);
-    errorFileds.push(errorField);
-  }
-  throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorFileds);
+const getErrorResponseBySequelizeValidationError = ({ errors }) => {
+  const errorFields = errors.map(
+    ({ path, value, message }) => new ErrorField(path, value, message),
+  );
+  return new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorFields);
 };
 
 export default getErrorResponseBySequelizeValidationError;
