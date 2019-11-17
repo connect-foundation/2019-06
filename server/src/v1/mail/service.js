933764af3a4c65958d64392e04574ae55a64d142
sjh2428 <pok_gare@naver.com>
2019-11-17 18:39:36 +0900

    feat: add transaction to send mail
    트랜잭션 추가

--- server/src/v1/mail/service.js
+++ server/src/v1/mail/service.js
@@ -2,45 +2,54 @@
 import nodemailer from 'nodemailer';
 import DB from '../../database/index';
 import U from '../../libraries/mail-util';
-import ERROR_CODE from '../../libraries/error-code';
-import ErrorResponse from '../../libraries/error-response';
 
 const getRawMails = async (userNo, userEmail) => {
   const mails = await DB.Mail.findAllReceivedMail(userNo, userEmail);
   return mails;
 };
 
-const saveAttachments = async (attachments, mailTemplateNo) => {
+const saveAttachments = async (attachments, mailTemplateNo, transaction) => {
   const processedAttachments = attachments.map(attachment => {
     const { type, name, content } = attachment;
     return { contentType: type, name, content, mail_template_id: mailTemplateNo };
   });
 
   try {
-    await DB.Attachment.bulkCreate(processedAttachments);
+    await DB.Attachment.bulkCreate(processedAttachments, { transaction });
   } catch (error) {
-    throw new ErrorResponse(ERROR_CODE.FAIL_TO_SAVE_ATTACHMENT);
+    // throw new ErrorResponse(ERROR_CODE.FAIL_TO_SAVE_ATTACHMENT); - 논의 후 수정
+    throw new Error(error);
   }
 };
 
-const saveMail = async mailContents => {
+const saveMail = async (mailContents, transaction) => {
   try {
-    const mailTemplateResult = await DB.MailTemplate.create(mailContents);
+    const mailTemplateResult = await DB.MailTemplate.create(mailContents, { transaction });
     const mailTemplate = mailTemplateResult.get({ plain: true });
-    const user = await DB.User.findOneById(mailContents.from.split('@')[0]);
-    await DB.Mail.create({
-      owner: user.no,
-      mail_template_id: mailTemplate.no,
-    });
-    await saveAttachments(mailContents.attachments, mailTemplate.no);
+    const user = await DB.User.findOneById(mailContents.from.split('@')[0], { transaction });
+    await DB.Mail.create(
+      {
+        owner: user.no,
+        mail_template_id: mailTemplate.no,
+      },
+      { transaction },
+    );
+    await saveAttachments(mailContents.attachments, mailTemplate.no, transaction);
   } catch (error) {
-    throw new ErrorResponse(ERROR_CODE.FAIL_TO_SAVE_MAIL);
+    // throw new ErrorResponse(ERROR_CODE.FAIL_TO_SAVE_MAIL); - 논의 후 수정
+    throw new Error(error);
   }
 };
 
 const sendMail = async mailContents => {
   const transporter = nodemailer.createTransport(U.getTransport());
-  await saveMail(mailContents);
+
+  try {
+    await DB.sequelize.transaction(async transaction => await saveMail(mailContents, transaction));
+  } catch (error) {
+    throw new Error(error);
+  }
+
   await transporter.sendMail(mailContents);
   return mailContents;
 };
