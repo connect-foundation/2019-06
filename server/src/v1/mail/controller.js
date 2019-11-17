623ffca836d21107f5bd698968019f1741fa77d3
sjh2428 <pok_gare@naver.com>
2019-11-17 21:03:05 +0900

    feat: add testcase for mail util and api
    메일 유틸 및 api에 관련하여 테스트케이스 추가

--- server/src/v1/mail/controller.js
+++ server/src/v1/mail/controller.js
@@ -1,6 +1,7 @@
 import STATUS from 'http-status';
 import service from './service';
 import U from '../../libraries/mail-util';
+import { validate } from '../../libraries/validator';
 import ERROR_CODE from '../../libraries/error-code';
 import ErrorResponse from '../../libraries/error-response';
 
@@ -20,6 +21,9 @@ const list = async (req, res, next) => {
 const write = async (req, res, next) => {
   const attachments = req.files;
   const { from, to, subject, text } = req.body;
+  if (!validate('email', to)) {
+    return next(new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE));
+  }
   const mailContents = U.getSingleMailData({ from, to, subject, text, attachments });
 
   let mail;
---
+++ server/test/mail/api.spec.js
@@ -0,0 +1,67 @@
+import request from 'supertest';
+import should from 'should';
+import app from '../../src/app';
+import DB from '../../src/database';
+import mock from '../../mock/create-dummy-data';
+
+const FROM_EMAIL = 'joajoa@daitnu.com';
+
+describe('Mail api test...', () => {
+  before(async () => {
+    await DB.sequelize.query('SET FOREIGN_KEY_CHECKS = 0');
+    await DB.sequelize.sync({ force: true });
+    await DB.sequelize.query('SET FOREIGN_KEY_CHECKS = 1');
+    await mock();
+  });
+  describe('로그인 하지 않은 상태로..', () => {
+    it('메일을 보내면 401에러를 반환한다', done => {
+      request(app)
+        .post('/v1/mail')
+        .send({
+          from: FROM_EMAIL,
+          to: 'rooot@daitnu.com',
+          subject: 'title이다',
+          text: 'body이다',
+          attachments: [],
+        })
+        .expect(401, done);
+    });
+  });
+  describe('로그인 한 상태로..', () => {
+    const userCredentials = {
+      id: 'rooot',
+      password: '12345678',
+    };
+    const authenticatedUser = request.agent(app);
+    before(done => {
+      authenticatedUser
+        .post('/v1/auth/login')
+        .send(userCredentials)
+        .expect(200, done);
+    });
+    it('보낼 사람의 이메일이 유효한 이메일이 아니라면 400 에러를 반환한다', done => {
+      authenticatedUser
+        .post('/v1/mail')
+        .send({
+          from: FROM_EMAIL,
+          to: 'yyyy@da.c',
+          subject: 'title이sdadweq다',
+          text: 'body이czxczx다',
+          attachments: [],
+        })
+        .expect(400, done);
+    });
+    it('보낼 사람의 이메일이 유효한 이메일이 아니라면 400 에러를 반환한다', done => {
+      authenticatedUser
+        .post('/v1/mail')
+        .send({
+          from: FROM_EMAIL,
+          to: 'qwewywq@dassass',
+          subject: 'titeqweqqele이다',
+          text: 'body이sdasdqw다',
+          attachments: [],
+        })
+        .expect(400, done);
+    });
+  });
+});
