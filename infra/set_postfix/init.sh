4fed94592466f030b6bec622e41ee89d3d27afb1
BuildTheTruth <dkwk1254@naver.com>
2019-11-18 12:28:05 +0900

    refactor: modify to set smtps, submissions
    정규식보다 해당 라인을 대체하는 코드로 변경

--- infra/set_postfix/init.sh
+++ infra/set_postfix/init.sh
@@ -33,28 +33,28 @@ postconf -e 'smtpd_tls_received_header = yes'
 postconf -e 'myhostname = mail.'$DOMAIN_NAME
 
 # set smtps, submission
-sed -i 's/#smtp      inet  n       -       y       -       -       smtpd/smtp      inet  n       -       y       -       -       smtpd/' $POSTFIX_MASTER
-sed -i 's/#submission inet n       -       y       -       -       smtpd/submission inet n       -       y       -       -       smtpd/' $POSTFIX_MASTER
-sed -i 's/#  -o syslog_name=postfix/  -o syslog_name=postfix/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_tls_security_level=encrypt/  -o smtpd_tls_security_level=encrypt/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_sasl_auth_enable=yes/  -o smtpd_sasl_auth_enable=yes/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_reject_unlisted_recipient=no/  -o smtpd_reject_unlisted_recipient=no/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_client_restrictions=$mua_client_restrictions/  -o smtpd_client_restrictions=$mua_client_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_helo_restrictions=$mua_helo_restrictions/  -o smtpd_helo_restrictions=$mua_helo_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_sender_restrictions=$mua_sender_restrictions/  -o smtpd_sender_restrictions=$mua_sender_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_recipient_restrictions=/  -o smtpd_recipient_restrictions=/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/' $POSTFIX_MASTER
-sed -i 's/#  -o milter_macro_daemon_name=ORIGINATING/  -o milter_macro_daemon_name=ORIGINATING/' $POSTFIX_MASTER
-sed -i 's/#smtps     inet  n       -       y       -       -       smtpd/smtps     inet  n       -       y       -       -       smtpd/' $POSTFIX_MASTER
-sed -i 's/#  -o syslog_name=postfix/  -o syslog_name=postfix/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_tls_wrappermode=yes/  -o smtpd_tls_wrappermode=yes/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_sasl_auth_enable=yes/  -o smtpd_sasl_auth_enable=yes/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_reject_unlisted_recipient=no/  -o smtpd_reject_unlisted_recipient=no/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_client_restrictions=$mua_client_restrictions/  -o smtpd_client_restrictions=$mua_client_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_helo_restrictions=$mua_helo_restrictions/  -o smtpd_helo_restrictions=$mua_helo_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_sender_restrictions=$mua_sender_restrictions/  -o smtpd_sender_restrictions=$mua_sender_restrictions/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_recipient_restrictions=/  -o smtpd_recipient_restrictions=/' $POSTFIX_MASTER
-sed -i 's/#  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/' $POSTFIX_MASTER
-sed -i 's/#  -o milter_macro_daemon_name=ORIGINATING/  -o milter_macro_daemon_name=ORIGINATING/' $POSTFIX_MASTER
+sed -i '12c\\smtp      inet  n       -       y       -       -       smtpd' $POSTFIX_MASTER
+sed -i '17c\\submission inet n       -       y       -       -       smtpd' $POSTFIX_MASTER
+sed -i '18c\\  -o syslog_name=postfix/submission' $POSTFIX_MASTER
+sed -i '19c\\  -o smtpd_tls_security_level=encrypt/' $POSTFIX_MASTER
+sed -i '20c\\  -o smtpd_sasl_auth_enable=yes/' $POSTFIX_MASTER
+sed -i '21c\\  -o smtpd_reject_unlisted_recipient=no/' $POSTFIX_MASTER
+sed -i '22c\\  -o smtpd_client_restrictions=$mua_client_restrictions/' $POSTFIX_MASTER
+sed -i '23c\\  -o smtpd_helo_restrictions=$mua_helo_restrictions/' $POSTFIX_MASTER
+sed -i '24c\\  -o smtpd_sender_restrictions=$mua_sender_restrictions/' $POSTFIX_MASTER
+sed -i '25c\\  -o smtpd_recipient_restrictions=/' $POSTFIX_MASTER
+sed -i '26c\\  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/' $POSTFIX_MASTER
+sed -i '27c\\  -o milter_macro_daemon_name=ORIGINATING/' $POSTFIX_MASTER
+sed -i '28c\\smtps     inet  n       -       y       -       -       smtpd/' $POSTFIX_MASTER
+sed -i '29c\\  -o syslog_name=postfix/smtps' $POSTFIX_MASTER
+sed -i '30c\\  -o smtpd_tls_wrappermode=yes/' $POSTFIX_MASTER
+sed -i '31c\\  -o smtpd_sasl_auth_enable=yes/' $POSTFIX_MASTER
+sed -i '32c\\  -o smtpd_reject_unlisted_recipient=no/' $POSTFIX_MASTER
+sed -i '33c\\  -o smtpd_client_restrictions=$mua_client_restrictions/' $POSTFIX_MASTER
+sed -i '34c\\  -o smtpd_helo_restrictions=$mua_helo_restrictions/' $POSTFIX_MASTER
+sed -i '35c\\  -o smtpd_sender_restrictions=$mua_sender_restrictions/' $POSTFIX_MASTER
+sed -i '36c\\  -o smtpd_recipient_restrictions=/' $POSTFIX_MASTER
+sed -i '37c\\  -o smtpd_relay_restrictions=permit_sasl_authenticated,reject/' $POSTFIX_MASTER
+sed -i '38c\\  -o milter_macro_daemon_name=ORIGINATING/' $POSTFIX_MASTER
 
 systemctl restart postfix.service
