146307a1d44b0988c4a5239090fc4ef7878a9787
BuildTheTruth <dkwk1254@naver.com>
2019-11-19 16:36:54 +0900

    refactor: add chmod, npm install in init.sh
    controller.sh, node-runner.sh에 파일 권한을 부여
    파일들이 복사된 후 node의 모듈들이 설치될 수 있도록 npm install 추가

--- infra/maildb/init.sh
+++ infra/maildb/init.sh
@@ -2,7 +2,11 @@
 
 INSPECT_DIR=/var/spool/filter
 
+chmod 750 controller.sh node-runner.sh
 cp -r ./* $INSPECT_DIR
 rm $INSPECT_DIR/init.sh
 chown filter:filter $INSPECT_DIR/*
+cd $INSPECT_DIR/src
+npm install
+
 echo "ready to receive mail"
