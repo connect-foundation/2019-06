7ac7d5d7304f00464f8f5d848c28cdee6e6cb3f9
sjh2428 <pok_gare@naver.com>
2019-11-19 13:49:21 +0900

    feat: add mail send using axios
    axios를 활용하여 메일 전송 완료

--- web/components/WriteMail/SubmitButton/index.js
+++ web/components/WriteMail/SubmitButton/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
@@ -9,23 +9,59 @@ import Popper from '@material-ui/core/Popper';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuList from '@material-ui/core/MenuList';
 import SendIcon from '@material-ui/icons/Send';
+import axios from 'axios';
 import { WriteMailContext } from '../ContextProvider';
-import GS from '../styled';
+import * as WM_S from '../styled';
+import { BASE_URL } from '../../../config/axios-config';
 
 const SubmitButton = () => {
   const { receivers } = useContext(WriteMailContext).receiver;
   const { subjectComponent, bodyComponent } = useContext(WriteMailContext);
+  const [sendMessage, setSendMessage] = useState(null);
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
 
   const handleClick = () => {
-    console.info('보내기 클릭');
+    setSendMessage(
+      <WM_S.RowWrapper>
+        <div></div>
+        <div>보내는 중...</div>
+      </WM_S.RowWrapper>,
+    );
+
+    axios
+      .post(
+        `${BASE_URL}/mail`,
+        {
+          to: receivers,
+          subject: subjectComponent.current.value,
+          text: bodyComponent.current.innerText,
+        },
+        { withCredentials: true },
+      )
+      .then(() => {
+        setSendMessage(
+          <WM_S.RowWrapper>
+            <div></div>
+            <div>메일 전송 완료</div>
+          </WM_S.RowWrapper>,
+        );
+      })
+      .catch(err => {
+        console.log(err);
+        setSendMessage(
+          <WM_S.RowWrapper>
+            <div></div>
+            <div>메일 전송 실패</div>
+          </WM_S.RowWrapper>,
+        );
+      });
     console.log(receivers);
     console.log(subjectComponent.current.value);
     console.log(bodyComponent.current.innerText);
   };
 
-  const handleMenuItemClick = event => {
+  const handleMenuItemClick = () => {
     console.log('보내기 예약 클릭');
     console.log(receivers);
     console.log(subjectComponent.current.value);
@@ -45,36 +81,44 @@ const SubmitButton = () => {
   };
 
   return (
-    <GS.RowWrapper>
-      <div></div>
-      <div>
-        <ButtonGroup variant="outlined" color="default" ref={anchorRef}>
-          <Button onClick={handleClick}>보내기</Button>
-          <Button color="default" size="small" onClick={handleToggle}>
-            <ArrowDropDownIcon />
-          </Button>
-        </ButtonGroup>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              {...TransitionProps}
-              style={{
-                transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
-              }}>
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList id="split-button-menu">
-                    <MenuItem onClick={event => handleMenuItemClick(event)}>
-                      {<SendIcon fontSize="small" />} 보내기 예약
-                    </MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </GS.RowWrapper>
+    <>
+      {sendMessage}
+      <WM_S.RowWrapper>
+        <div></div>
+        <div>
+          <ButtonGroup variant="outlined" color="default" ref={anchorRef}>
+            <Button onClick={handleClick}>보내기</Button>
+            <Button color="default" size="small" onClick={handleToggle}>
+              <ArrowDropDownIcon />
+            </Button>
+          </ButtonGroup>
+          <Popper
+            open={open}
+            anchorEl={anchorRef.current}
+            role={undefined}
+            transition
+            disablePortal>
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                style={{
+                  transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom',
+                }}>
+                <Paper>
+                  <ClickAwayListener onClickAway={handleClose}>
+                    <MenuList id="split-button-menu">
+                      <MenuItem onClick={event => handleMenuItemClick(event)}>
+                        {<SendIcon fontSize="small" />} 보내기 예약
+                      </MenuItem>
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      </WM_S.RowWrapper>
+    </>
   );
 };
 
