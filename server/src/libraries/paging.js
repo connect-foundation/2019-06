14f09b2e0d73592844a14dfa56cfe54dc01ec8fb
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-19 21:16:27 +0900

    feat(be/paging): paging 함수구현 메일리스트의 페이징을 startpage,endpage를 구한다.

---
+++ server/src/libraries/paging.js
@@ -0,0 +1,29 @@
+const PAGE_LIST_NUM = 10;
+
+/**
+ * @param {Number} totalCount 메일함에있는 메일
+ * @param {Object} options
+ * @param {Number} options.page 현재 페이지
+ * @param {Number} options.pageNum 한페이지당 출력할 갯수
+ * @returns {Object} paging result
+ */
+
+function paging(totalCount, options = {}) {
+  const pageNum = options.pageNum || 100;
+  let page = options.page || 1;
+
+  let totalPage = Math.floor(totalCount / pageNum);
+  totalPage = totalCount % pageNum ? totalPage + 1 : totalPage;
+  totalPage = totalPage === 0 ? 1 : totalPage;
+
+  if (totalPage < page) {
+    page = totalPage;
+  }
+
+  const startPage = Math.floor((page - 1) / PAGE_LIST_NUM) * PAGE_LIST_NUM + 1;
+  let endPage = startPage + PAGE_LIST_NUM - 1;
+  endPage = endPage > totalPage ? totalPage : endPage;
+  return { startPage, endPage, page, pageNum, totalPage };
+}
+
+export default paging;
---
+++ server/test/libraries/paging.spec.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+import should from 'should';
+import paging from '../../src/libraries/paging';
+
+describe('Paging Test...', () => {
+  it('# 페이징결과를 객체로 반환한다.', () => {
+    const totalCount = 10000;
+    const options = {
+      page: 1,
+      pageNum: 100,
+    };
+    const result = paging(totalCount, options);
+    result.should.be.properties('startPage', 'endPage', 'page', 'pageNum', 'totalPage');
+  });
+
+  it('# page의 default는 1로 계산된다.', () => {
+    const totalCount = 10000;
+    const options = {
+      pageNum: 100,
+    };
+    const result = paging(totalCount, options);
+    result.page.should.be.equal(1);
+  });
+
+  it('# page와 pageNum은 입력된 값대로 반환된다.', () => {
+    const totalCount = 10000;
+    const options = {
+      pageNum: 100,
+      page: 5,
+    };
+    const result = paging(totalCount, options);
+    result.page.should.be.equal(5);
+    result.pageNum.should.be.equal(100);
+  });
+
+  it('# pagesms startPage와 endPage 사이의 값이다.', () => {
+    const totalCount = 10000;
+    const options = {
+      pageNum: 100,
+      page: 10,
+    };
+    const result = paging(totalCount, options);
+    const { page, startPage, endPage } = result;
+    page.should.be.within(startPage, endPage);
+  });
+
+  it('# totalCount가 0이면 page, startpage, endpage가 1이다.', () => {
+    const totalCount = 0;
+    const options = {
+      pageNum: 100,
+      page: 1,
+    };
+    const result = paging(totalCount, options);
+    const { page, startPage, endPage } = result;
+    page.should.be.equals(1);
+    startPage.should.be.equals(1);
+    endPage.should.be.equals(1);
+  });
+
+  it('# totalCount가 101이고 pageNum이 100이면 endPage는 2이다.', () => {
+    const totalCount = 101;
+    const options = {
+      pageNum: 100,
+      page: 1,
+    };
+    const result = paging(totalCount, options);
+    const { page, endPage } = result;
+    page.should.be.equals(1);
+    endPage.should.be.equals(2);
+  });
+});
