29eda1434cb9cc84ae52d86b3f718a6292bce9c0
Jin <dkwk1254@naver.com>
2019-11-20 23:13:33 +0900

    feat(be/category): 회원가입 시 Category 테이블에 default 카테고리 데이터 생성
    회원가입 query와 default 카테고리를 생성해주는 query를 transaction으로 묶음

--- server/src/v1/users/service.js
+++ server/src/v1/users/service.js
@@ -2,19 +2,37 @@ import DB from '../../database';
 import ErrorResponse from '../../libraries/error-response';
 import ERROR_CODE from '../../libraries/error-code';
 
+const DEFAULT_CATEGORIES = ['전체메일함', '받은메일함', '보낸메일함', '내게쓴메일함', '휴지통'];
+
 // eslint-disable-next-line camelcase
-const register = async ({ user_id, password, name, sub_email }) => {
-  const userData = { user_id, password, name, sub_email };
+const register = async ({ id, password, name, sub_email }) => {
+  const userData = { id, password, name, sub_email };
+  let newUser;
 
-  const [response, created] = await DB.User.findOrCreateById(userData);
-
-  if (!created) {
-    throw new ErrorResponse(ERROR_CODE.ID_DUPLICATION);
+  try {
+    await DB.sequelize.transaction(async transaction => {
+      const [response, created] = await DB.User.findOrCreateById(userData, { transaction });
+      if (!created) {
+        throw new ErrorResponse(ERROR_CODE.ID_DUPLICATION);
+      }
+      newUser = response.get({ plain: true });
+      await createDefaultCategories(newUser.no, transaction);
+    });
+  } catch (error) {
+    throw error;
   }
 
-  const newUser = response.get({ plain: true });
   delete newUser.password;
   return newUser;
 };
 
-export default { register };
+const createDefaultCategories = async (no, transaction) => {
+  const categories = DEFAULT_CATEGORIES.map(name => ({
+    user_no: no,
+    is_default: true,
+    name,
+  }));
+  await DB.Category.bulkCreate(categories, { transaction });
+};
+
+export default { register, createDefaultCategories };
