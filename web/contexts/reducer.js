b4320ae67d0d788d249d1b02ea951a92d1e21558
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-22 01:55:33 +0900

    feat: 전역컨텍스트 메일체인지 액션 생성 state가 변할때 mails를 변경하기 위한 액션

--- web/contexts/reducer.js
+++ web/contexts/reducer.js
@@ -1,24 +1,32 @@
-import axios from 'axios';
-
 export const initialState = {
   category: 0,
   page: 1,
   mails: null,
+  paging: null,
 };
 
 const CATEGORY_CLICK = 'CATEGORY_CLICK';
 const PAGE_NUMBER_CLICK = 'PAGE_NUMBER_CLICK';
+const CHANGE_MAILS_DATA = 'CHANGE_MAILS_DATA';
 
-export const handleCategoryClick = async category => {
-  const URL = `/v1/mail?category=${category}`;
-  const mails = await axios.get(URL);
-
+export const handleCategoryClick = ({ category, mails, paging, page }) => {
   return {
     type: CATEGORY_CLICK,
     payload: {
       category,
       mails,
       page: 1,
+      paging,
+    },
+  };
+};
+
+export const handleMailsChange = ({ mails, paging }) => {
+  return {
+    type: CHANGE_MAILS_DATA,
+    payload: {
+      mails,
+      paging,
     },
   };
 };
@@ -38,6 +46,8 @@ export const reducer = (state = initialState, action) => {
     case CATEGORY_CLICK:
       return { ...state, ...payload };
     case PAGE_NUMBER_CLICK:
+      return { ...state, ...payload };
+    case CHANGE_MAILS_DATA:
       return { ...state, ...payload };
     default:
       return { ...state };
