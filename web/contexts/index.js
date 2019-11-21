ac1ff47ff241dca8598623c82d60c988a41a6f16
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-21 14:46:00 +0900

    fix(fe): context에 불필요한 변수 제거

--- web/contexts/index.js
+++ web/contexts/index.js
@@ -2,7 +2,6 @@ import React, { useReducer, createContext } from 'react';
 import { reducer, initialState } from './reducer';
 
 const AppContext = createContext();
-const { Provider } = AppContext;
 
 const AppProvider = props => {
   const [state, dispatch] = useReducer(reducer, initialState);
