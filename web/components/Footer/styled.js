c813ec91f094a4cb9a9f4c8e2221dcf2f4c0b1bb
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-21 15:05:23 +0900

    fix: footer 중앙정렬 top에 붙어있는것을 중앙으로 내려서 심리적 안정을 꾀함

--- web/components/Footer/styled.js
+++ web/components/Footer/styled.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 const Footer = styled.footer`
   flex: 0 0 30px;
   border-top: 2px solid #e9ecef;
-  text-align: center;
   width: 100%;
   bottom: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default { Footer };
