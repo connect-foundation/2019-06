68b22efee975136384ae7b3215aff31793b77dc8
hwanlee <ljhw3377@gmail.com>
2019-11-22 11:55:23 +0900

    add review code

---
+++ review/frontend/index.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as GS from '../components/GlobalStyle';
+import Aside from '../components/Aside';
+import MailArea from '../components/MailArea';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [view, setView] = useState(<MailArea />);
+
+  useEffect(() => {
+    if (!window.sessionStorage.getItem('user')) {
+      router.push('/login');
+    } else {
+      setLoading(true);
+    }
+  }, []);
+
+  if (!loading) {
+    return <div></div>;
+  }
+
+  return (
+    <GS.FlexWrap>
+      <Header brand={'Daitnu'} />
+      <GS.Content>
+        <Aside setView={setView} />
+        {view}
+      </GS.Content>
+      <Footer />
+    </GS.FlexWrap>
+  );
+};
+
+export default Home;
