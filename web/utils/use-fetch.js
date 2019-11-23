5e391fa0a5b50ceffd890a11c141b40084425419
Jin <dkwk1254@naver.com>
2019-11-23 21:13:00 +0900

    refactor: useFetch에 query를 매개변수로 받을 수 있도록 변경
    url에서 query 문자열을 분리하여 url 구성을 더 간단하게 할 수 있도록 함

--- web/utils/use-fetch.js
+++ web/utils/use-fetch.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useFetch = (callback, url) => {
+const useFetch = (callback, url, query) => {
   const [loading, setLoading] = useState(true);
+  const queryStr = Object.entries(query)
+    .map(([k, v]) => `${k}=${v}`)
+    .reduce((a, b) => `${a}&${b}`);
 
   const fetchInitialData = async () => {
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(`${url}?${queryStr}`);
     callback(data);
     setLoading(false);
   };
