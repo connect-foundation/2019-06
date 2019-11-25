375f99babe766a4a22eb768721b74ee14fe43e5f
hwanlee <ljhw3377@gmail.com>
2019-11-25 13:25:27 +0900

    feat(fe/user): 프로필 화면으로 이동하는 링크 컴포넌트

---
+++ web/components/ProfileLink/styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Brand = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  margin: 20px;
+  cursor: pointer;
+`;
+
+const Logo = styled.img`
+  width: 50px;
+  height: 50px;
+`;
+
+export default {
+  Brand,
+  Logo,
+};
