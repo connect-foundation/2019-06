d2156b735ad7eac4a81d464fb6cd80b3064a68ec
hwanlee <ljhw3377@gmail.com>
2019-11-26 10:28:09 +0900

    fix(be): 에러 처리 수정
    ErrorResponse에서 배열이 아닌 필드를 넘겨줄 경우 배열으로 바꿔줌

--- server/src/libraries/validation/user.js
+++ server/src/libraries/validation/user.js
@@ -20,16 +20,13 @@ const join = async body => {
 };
 
 const checkQueryForSearch = ({ type }) => {
-  const errorFields = [];
-
   if (!type || (type !== 'id' && type !== 'pw')) {
     const errorField = new ErrorField(
       'type',
       type,
       'type값은 아이디를 찾을 경우 "id" 비밀번호를 찾을 경우 "pw"여야 합니다.',
     );
-    errorFields.push(errorField);
-    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorFields);
+    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorField);
   }
 
   return true;
@@ -38,7 +35,7 @@ const checkQueryForSearch = ({ type }) => {
 const checkBodyForPasswordUpdate = ({ password }) => {
   if (!password || !validate('password', password)) {
     const errorField = new ErrorField('password', password, 'password 값이 올바르지 않습니다.');
-    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, [errorField]);
+    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorField);
   }
 
   return true;
@@ -47,7 +44,7 @@ const checkBodyForPasswordUpdate = ({ password }) => {
 const checkBodyForIdSearch = ({ email }) => {
   if (!email || !validate('email', email)) {
     const errorField = new ErrorField('email', email, 'email 값이 올바르지 않습니다.');
-    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, [errorField]);
+    throw new ErrorResponse(ERROR_CODE.INVALID_INPUT_VALUE, errorField);
   }
 
   return true;
