c78646a9db8822df9c8e15f84443141fb58112b5
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-11-26 14:01:05 +0900

    fix: mailTemplate icon변경 이쁘게 바꿈

--- web/components/MailTemplate/index.js
+++ web/components/MailTemplate/index.js
@@ -1,24 +1,61 @@
 /* eslint-disable camelcase */
 import React, { useContext } from 'react';
 import moment from 'moment';
-import * as S from './styled';
-import { AppDisapthContext } from '../../contexts';
-import { handleMailClick } from '../../contexts/reducer';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import StarIcon from '@material-ui/icons/Star';
+import MailIcon from '@material-ui/icons/Mail';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { makeStyles } from '@material-ui/core/styles';
+import { red, yellow } from '@material-ui/core/colors';
 import ReadMail from '../ReadMail';
+import { handleMailClick } from '../../contexts/reducer';
+import { AppDisapthContext } from '../../contexts';
+import * as S from './styled';
+
+const useStyles = makeStyles(theme => ({
+  delete: {
+    '&:hover': {
+      color: red[800],
+    },
+  },
+  unstar: {
+    color: '#808080',
+    '&:hover': {
+      color: yellow[800],
+    },
+  },
+  star: {
+    color: yellow[800],
+    '&:hover': {
+      color: '#1976d2',
+    },
+  },
+}));
 
 const MailTemplate = ({ mail, no }) => {
   const { dispatch } = useContext(AppDisapthContext);
   const { from, subject, date, is_important, is_read } = mail;
   const startdate = moment(date).format('YYYY-MM-DD');
   const handleSubjectClick = () => dispatch(handleMailClick(mail, <ReadMail />));
+  const classes = useStyles();
+
   return (
-    <S.MailTemplateWrap>
+    <S.MailTemplateWrap isRead={is_read}>
       <div>
-        {no}
-        {/* <input type="checkbox" /> */}
+        <input type="checkbox" />
       </div>
-      <div>{is_important ? 'O' : 'X'}</div>
-      <div>{is_read ? 'O' : 'X'}</div>
+      <div>
+        {is_important ? (
+          <StarIcon className={classes.star} />
+        ) : (
+          <StarBorderIcon className={classes.unstar} />
+        )}
+      </div>
+      <div>{is_read ? <DraftsIcon /> : <MailIcon />}</div>
+      <div>
+        <DeleteIcon className={classes.delete} />
+      </div>
       <div>{from}</div>
       <div onClick={handleSubjectClick}>{subject}</div>
       <div>{startdate}</div>
--- web/components/MailTemplate/styled.js
+++ web/components/MailTemplate/styled.js
@@ -6,40 +6,57 @@ const MailTemplateWrap = styled.div`
   flex-direction: row;
   align-items: center;
   padding: 0 0 0 10px;
-  cursor: pointer;
+  font-size: 0.8rem;
+  font-weight: ${props => (props.isRead ? '400' : 'bold')};
   & + & {
     border-top: 2px solid #eceff1;
-  }
-  &:hover {
-    border-left: 2px solid #eceff1;
-    border-right: 2px solid #eceff1;
   }
 
   & > div {
     &:nth-child(1) {
-      flex: 0 0 20px;
+      flex: 0 0 30px;
     }
 
     &:nth-child(2) {
-      flex: 0 0 50px;
+      flex: 0 0 40px;
+      cursor: pointer;
     }
 
     &:nth-child(3) {
-      flex: 0 0 50px;
+      flex: 0 0 40px;
     }
 
     &:nth-child(4) {
+      flex: 0 0 40px;
+      cursor: pointer;
+    }
+
+    &:nth-child(5) {
       flex: 0 0 150px;
       text-overflow: ellipsis;
       white-space: nowrap;
       overflow: hidden;
+      cursor: pointer;
+      color: ${props => (props.isRead ? 'grey' : '#0459C1')};
+      &:hover {
+        text-decoration: underline;
+      }
     }
 
-    &:nth-child(5) {
+    &:nth-child(6) {
       flex: 1 1;
       margin-left: 15px;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+      cursor: pointer;
+      color: ${props => (props.isRead ? 'grey' : '#0459C1')};
+      &:hover {
+        text-decoration: underline;
+      }
     }
-    &:nth-child(6) {
+
+    &:nth-child(7) {
       flex: 0 0 100px;
     }
   }
