d55494c5e0e3227362f38341c50a1fbfbef053f7
sjh2428 <pok_gare@naver.com>
2019-11-26 15:51:38 +0900

    test(be): 메일함 삭제 요청시 테스트 코드 추가
    name에 빈 값('')을 넘길 경우

--- server/test/mail_box/api.spec.js
+++ server/test/mail_box/api.spec.js
@@ -156,6 +156,7 @@ describe('mailbox api test...', () => {
           .send()
           .expect(400, done);
       });
+
       it('넘겨지는 데이터가 name, no중 하나라도 없으면 400에러를 반환한다.', done => {
         authenticatedUser
           .delete('/v1/mail/box')
@@ -164,6 +165,13 @@ describe('mailbox api test...', () => {
         authenticatedUser
           .delete('/v1/mail/box')
           .send({ no: 9 })
+          .expect(400, done);
+      });
+
+      it('name을 빈값을 넘기면 400에러를 반환한다.', done => {
+        authenticatedUser
+          .delete('/v1/mail/box')
+          .send({ no: 9, name: '' })
           .expect(400, done);
       });
     });
