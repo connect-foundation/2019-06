48891589f33228490dc9d0c18d5f197679500fc1
hwanlee <ljhw3377@gmail.com>
2019-11-26 23:15:30 +0900

    fix(fe): 불필요한 모듈 import 제거

--- web/components/Forms/RegisterForm/index.js
+++ web/components/Forms/RegisterForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState } from 'react';
 import Router from 'next/router';
 import { TextField, OutlinedInput, InputAdornment, IconButton } from '@material-ui/core';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
