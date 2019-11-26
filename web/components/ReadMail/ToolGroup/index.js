c82e5a517f5dd9a92ecbbc38d95275780d27b6dc
Jin <dkwk1254@naver.com>
2019-11-26 20:20:07 +0900

    feat(fe/read-mail/tool-group): 메일 읽기 화면에서 보이는 tool group 컴포넌트 생성
    답장, 전체답장, 전달, 삭제 기능들 제공할 예정

---
+++ web/components/ReadMail/ToolGroup/index.js
@@ -0,0 +1,8 @@
+import React, { useContext } from 'react';
+import * as S from './styled';
+
+const ToolGroup = () => {
+  return <S.Container></S.Container>;
+};
+
+export default ToolGroup;
---
+++ web/components/ReadMail/ToolGroup/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex: 0 0 50px;
+  border-bottom: 2px solid #e9ecef;
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { Container };
