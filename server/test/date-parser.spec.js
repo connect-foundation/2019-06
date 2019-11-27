427572be84a77bd6e08259466ea8df923a27c663
hwanlee <ljhw3377@gmail.com>
2019-11-27 17:48:12 +0900

    fix: describe.only 제거

--- server/test/date-parser.spec.js
+++ server/test/date-parser.spec.js
@@ -1,7 +1,7 @@
 import should from 'should';
 import { strToDate } from '../src/libraries/date-parser';
 
-describe.only('strToDate 함수는..', () => {
+describe('strToDate 함수는..', () => {
   it('# "YYYY:MM:DD HH:mm" 형태의 문자열을 넘겨줄 경우 Date 형식으로 넘겨준다.', () => {
     const str = '2000:12:01 03:00';
 
