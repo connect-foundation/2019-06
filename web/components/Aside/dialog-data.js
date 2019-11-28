39c3b93335cb571288263de46bcd47209e40dbd9
sjh2428 <pok_gare@naver.com>
2019-11-28 15:47:56 +0900

    feat(fe): 메일함명 변경 서버와 통신 코드 추가
    메일함의 이름 변경 요청 코드 추가

--- web/components/Aside/dialog-data.js
+++ web/components/Aside/dialog-data.js
@@ -5,10 +5,6 @@ import req from '../../utils/request';
 const [ADD, MODIFY, DELETE] = [0, 1, 2];
 const url = '/mail/box';
 
-const makeMailbox = e => {};
-
-const alterMailbox = e => {};
-
 const deleteMailbox = e => {};
 
 export const getDialogData = (type, customCategory, idx, setDialogOpen, setCustomCategory) => {
@@ -18,7 +14,7 @@ export const getDialogData = (type, customCategory, idx, setDialogOpen, setCusto
         title: '메일함 추가',
         textContents: '추가할 메일함 이름을 적어주세요',
         needTextField: true,
-        okBtnHandler: async (e, name) => {
+        okBtnHandler: async (_, name) => {
           if (customCategory.every(category => category.name === name)) {
             // TODO: 상단에 에러 메세지 보여주기 (메일함은 이름을 중복해서 만들 수 없습니다)
             return;
@@ -43,13 +39,23 @@ export const getDialogData = (type, customCategory, idx, setDialogOpen, setCusto
         title: `메일함명(${customCategory[idx].name}) 변경`,
         textContents: '변경할 메일함 이름을 적어주세요',
         needTextField: true,
-        okBtnHandler: (e, name) => {
-          if (customCategory.every(category => category.name !== name)) {
-            customCategory[idx].name = name;
-            setCustomCategory([...customCategory]);
-            // 서버와 통신하여 메일함 이름을 바꾸어야 함
-            alterMailbox(e);
+        okBtnHandler: async (_, name) => {
+          if (customCategory.every(category => category.name === name)) {
+            // TODO: 상단에 에러 메세지 보여주기 (메일함은 이름을 중복해서 만들 수 없습니다)
+            return;
           }
+          const { isError } = await req.patch(url, {
+            oldName: customCategory[idx].name,
+            newName: name,
+            no: customCategory[idx].no,
+          });
+          if (isError) {
+            console.log(isError);
+            // TODO: 상단에 에러 메세지 보여주기 (data.message)
+            return;
+          }
+          customCategory[idx].name = name;
+          setCustomCategory([...customCategory]);
           setDialogOpen(false);
         },
       };
