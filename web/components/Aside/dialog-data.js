0395d67f18abc6dac4cfe83f5d2c8b18a9532ba1
sjh2428 <pok_gare@naver.com>
2019-11-28 15:53:39 +0900

    feat(fe): 메일함 삭제 요청 코드 추가
    BE에 인프라와 DB에 존재하는 메일함 삭제 요청 코드 추가

--- web/components/Aside/dialog-data.js
+++ web/components/Aside/dialog-data.js
@@ -3,9 +3,7 @@ import { StarBorder } from '@material-ui/icons';
 import req from '../../utils/request';
 
 const [ADD, MODIFY, DELETE] = [0, 1, 2];
-const url = '/mail/box';
-
-const deleteMailbox = e => {};
+const url = '/mail/box/';
 
 export const getDialogData = (type, customCategory, idx, setDialogOpen, setCustomCategory) => {
   switch (type) {
@@ -64,11 +62,17 @@ export const getDialogData = (type, customCategory, idx, setDialogOpen, setCusto
         title: `메일함(${customCategory[idx].name}) 삭제`,
         textContents: '정말로 삭제하시겠습니까?',
         needTextField: false,
-        okBtnHandler: e => {
+        okBtnHandler: async e => {
+          const { no, name } = customCategory[idx];
+          const query = `no=${no}&name=${name}`;
+          const { isError } = await req.delete(`${url}?${query}`);
+          if (isError) {
+            console.log(isError);
+            // TODO: 상단에 에러 메세지 보여주기 (data.message)
+            return;
+          }
           setCustomCategory(customCategory.filter((_, index) => idx !== index));
           setDialogOpen(false);
-          // 서버와 통신하여 메일함을 삭제해야 함
-          deleteMailbox(e);
         },
       };
 
