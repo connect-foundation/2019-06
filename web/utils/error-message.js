19fc91bbe1c500ae5a51d25a316020f91f8e526b
hwanlee <ljhw3377@gmail.com>
2019-11-30 20:46:26 +0900

    fix: 예약 날짜 에러 메시지 수정

--- web/utils/error-message.js
+++ web/utils/error-message.js
@@ -3,7 +3,7 @@ const ERROR_ID_VALIDATION = '잘못된 아이디입니다';
 const ERROR_PASSWORD_EMPTY = '비밀번호가 입력되지 않았습니다';
 const ERROR_PASSWORD_VALIDATION = '잘못된 비밀번호입니다.';
 const ERROR_DIFFERENT_PASSWORD = '같은 비밀번호를 입력해주세요.';
-const ERROR_CANNOT_RESERVATION = '이미 지난 날짜는 예약 메일을 전송할 수 없습니다.';
+const ERROR_CANNOT_RESERVATION = '이미 지난 날짜입니다.';
 
 export {
   ERROR_ID_EMPTY,
