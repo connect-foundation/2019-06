8cc028df81fd2d20a698dd7e7fe8f995047e20d6
hwanlee <ljhw3377@gmail.com>
2019-11-30 20:42:31 +0900

    feat(fe): 예약 메일 날짜 전송시 날짜 포맷팅을 위한 함수

--- web/utils/format.js
+++ web/utils/format.js
@@ -2,8 +2,16 @@ import moment from 'moment';
 
 const formatDateForReservationTimePicker = date => date.format('YYYY. MM. DD.');
 
+const formatDateToRequestReservation = date => {
+  return moment(date).format('YYYY:MM:DD HH:mm');
+};
+
 const formatDateForReservationDateText = date => {
   return moment(date).format('YYYY:MM:DD A hh:mm');
 };
 
-export { formatDateForReservationTimePicker, formatDateForReservationDateText };
+export {
+  formatDateForReservationTimePicker,
+  formatDateForReservationDateText,
+  formatDateToRequestReservation,
+};
