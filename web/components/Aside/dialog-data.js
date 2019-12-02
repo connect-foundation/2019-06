878a7dfd0da90065c65fabe5629700040b7cbb63
sjh2428 <pok_gare@naver.com>
2019-12-02 19:58:55 +0900

    feat(fe): 메일함 수정 에러 스낵바 추가

--- web/components/Aside/dialog-data.js
+++ web/components/Aside/dialog-data.js
@@ -4,6 +4,14 @@ const [ADD, MODIFY, DELETE] = [0, 1, 2];
 const url = '/mail/box/';
 const nameRegex = /^[0-9a-zA-Z가-힣 ]{1,20}$/;
 
+const [SNACKBAR_ERROR, SNACKBAR_SUCCESS] = ['error', 'success'];
+const duplicateErrorMsg = '메일함은 이름을 중복해서 만들 수 없습니다.';
+const lengthErrorMsg = '메일함 이름은 최대 20자를 넘을 수 없습니다.';
+const regexErrorMsg = '메일함은 완성된 한글, 영문, 숫자로만 이루어질 수 있습니다.';
+const serverErrorMsg = '';
+const successToAddMsg = '메일함이 성공적으로 추가되었습니다.';
+const successToModifyMsg = '메일함이 성공적으로 수정되었습니다.';
+
 export const getDialogData = (
   type,
   customCategory,
@@ -22,24 +30,24 @@ export const getDialogData = (
           if (customCategory.find(category => category.name === name)) {
             setSnackbarState({
               open: true,
-              variant: 'error',
-              contentText: '메일함은 이름을 중복해서 만들 수 없습니다.',
+              variant: SNACKBAR_ERROR,
+              contentText: duplicateErrorMsg,
             });
             return;
           }
           if (name.length > 20) {
             setSnackbarState({
               open: true,
-              variant: 'error',
-              contentText: '메일함 이름은 최대 20자를 넘을 수 없습니다.',
+              variant: SNACKBAR_ERROR,
+              contentText: lengthErrorMsg,
             });
             return;
           }
           if (!nameRegex.test(name)) {
             setSnackbarState({
               open: true,
-              variant: 'error',
-              contentText: '메일함은 완성된 한글, 영문, 숫자로만 이루어질 수 있습니다.',
+              variant: SNACKBAR_ERROR,
+              contentText: regexErrorMsg,
             });
             return;
           }
@@ -47,7 +55,7 @@ export const getDialogData = (
           if (isError) {
             setSnackbarState({
               open: true,
-              variant: 'error',
+              variant: SNACKBAR_ERROR,
               contentText: '서버와 통신에 실패하였습니다. 관리자에게 문의해주세요.',
             });
             return;
@@ -59,8 +67,8 @@ export const getDialogData = (
           });
           setSnackbarState({
             open: true,
-            variant: 'success',
-            contentText: '성공적으로 메일함이 추가되었습니다.',
+            variant: SNACKBAR_SUCCESS,
+            contentText: successToAddMsg,
           });
           setDialogOpen(false);
         },
@@ -73,14 +81,29 @@ export const getDialogData = (
         okBtnHandler: async (name, setSnackbarState) => {
           if (customCategory.find(category => category.name === name)) {
             // TODO: 상단에 에러 메세지 보여주기 (메일함은 이름을 중복해서 만들 수 없습니다)
+            setSnackbarState({
+              open: true,
+              variant: SNACKBAR_ERROR,
+              contentText: duplicateErrorMsg,
+            });
             return;
           }
           if (name.length > 20) {
             // TODO: 상단에 에러 메세지 보여주기 (메일함은 최대 20자를 넘을 수 없습니다)
+            setSnackbarState({
+              open: true,
+              variant: SNACKBAR_ERROR,
+              contentText: lengthErrorMsg,
+            });
             return;
           }
           if (!nameRegex.test(name)) {
             // TODO: 상단에 에러 메세지 보여주기 (메일함은 완성된 한글, 영문, 숫자로만 이루어질 수 있습니다)
+            setSnackbarState({
+              open: true,
+              variant: SNACKBAR_ERROR,
+              contentText: regexErrorMsg,
+            });
             return;
           }
           const { isError } = await request.patch(url + customCategory[idx].no, {
@@ -90,10 +113,20 @@ export const getDialogData = (
           if (isError) {
             console.log(isError);
             // TODO: 상단에 에러 메세지 보여주기 (data.message)
+            setSnackbarState({
+              open: true,
+              variant: SNACKBAR_ERROR,
+              contentText: '서버와 통신에 실패하였습니다. 관리자에게 문의해주세요.',
+            });
             return;
           }
           customCategory[idx].name = name;
           dispatch(setCustomCategory({ categories: customCategory }));
+          setSnackbarState({
+            open: true,
+            variant: SNACKBAR_ERROR,
+            contentText: successToModifyMsg,
+          });
           setDialogOpen(false);
         },
       };
