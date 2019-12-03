f60128de36988ac2a13b5e7757718662a583ad5f
sjh2428 <pok_gare@naver.com>
2019-12-03 09:55:29 +0900

    fix(fe): snackbar 문자열 객체화

--- web/components/Aside/dialog-data.js
+++ web/components/Aside/dialog-data.js
@@ -5,18 +5,28 @@ const [ADD, MODIFY, DELETE] = [0, 1, 2];
 const url = '/mail/box/';
 const nameRegex = /^[0-9a-zA-Z가-힣 ]{1,20}$/;
 
-const [SNACKBAR_ERROR, SNACKBAR_SUCCESS] = ['error', 'success'];
-const duplicateErrorMsg = '메일함은 이름을 중복해서 만들 수 없습니다.';
-const lengthErrorMsg = '메일함 이름은 최대 20자를 넘을 수 없습니다.';
-const regexErrorMsg = '메일함은 완성된 한글, 영문, 숫자로만 이루어질 수 있습니다.';
-const successToAddMsg = '메일함이 성공적으로 추가되었습니다.';
-const successToModifyMsg = '메일함이 성공적으로 수정되었습니다.';
-const successToDeleteMsg = '메일함이 성공적으로 삭제되었습니다.';
+const SNACKBAR_VARIANT = {
+  ERROR: 'error',
+  SUCCESS: 'success',
+};
+
+const SNACKBAR_MSG = {
+  ERROR: {
+    DUPLICATE: '메일함은 이름을 중복해서 만들 수 없습니다.',
+    LENGTH: '메일함 이름은 최대 20자를 넘을 수 없습니다.',
+    REGEX: '메일함은 완성된 한글, 영문, 숫자로만 이루어질 수 있습니다.',
+  },
+  SUCCESS: {
+    ADD: '메일함이 성공적으로 추가되었습니다.',
+    MODIFY: '메일함이 성공적으로 수정되었습니다.',
+    DELETE: '메일함이 성공적으로 삭제되었습니다.',
+  },
+};
 
 const [ERROR, SUCCESS] = [true, false];
 const getSnackbarState = (isError, contentText) => ({
   open: true,
-  variant: isError ? SNACKBAR_ERROR : SNACKBAR_SUCCESS,
+  variant: isError ? SNACKBAR_VARIANT.ERROR : SNACKBAR_VARIANT.SUCCESS,
   contentText,
 });
 
@@ -36,15 +46,15 @@ export const getDialogData = (
         needTextField: true,
         okBtnHandler: async (name, setSnackbarState) => {
           if (customCategory.find(category => category.name === name)) {
-            setSnackbarState(getSnackbarState(ERROR, duplicateErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.DUPLICATE));
             return;
           }
           if (name.length > 20) {
-            setSnackbarState(getSnackbarState(ERROR, lengthErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.LENGTH));
             return;
           }
           if (!nameRegex.test(name)) {
-            setSnackbarState(getSnackbarState(ERROR, regexErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.REGEX));
             return;
           }
           const { isError, data } = await request.post(url, { name });
@@ -58,7 +68,7 @@ export const getDialogData = (
             name: createdName,
             no,
           });
-          setSnackbarState(getSnackbarState(SUCCESS, successToAddMsg));
+          setSnackbarState(getSnackbarState(SUCCESS, SNACKBAR_MSG.SUCCESS.ADD));
           setDialogOpen(false);
         },
       };
@@ -69,15 +79,15 @@ export const getDialogData = (
         needTextField: true,
         okBtnHandler: async (name, setSnackbarState) => {
           if (customCategory.find(category => category.name === name)) {
-            setSnackbarState(getSnackbarState(ERROR, duplicateErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.DUPLICATE));
             return;
           }
           if (name.length > 20) {
-            setSnackbarState(getSnackbarState(ERROR, lengthErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.LENGTH));
             return;
           }
           if (!nameRegex.test(name)) {
-            setSnackbarState(getSnackbarState(ERROR, regexErrorMsg));
+            setSnackbarState(getSnackbarState(ERROR, SNACKBAR_MSG.ERROR.REGEX));
             return;
           }
           const { isError, data } = await request.patch(url + customCategory[idx].no, {
@@ -91,7 +101,7 @@ export const getDialogData = (
           }
           customCategory[idx].name = name;
           dispatch(setCustomCategory({ categories: customCategory }));
-          setSnackbarState(getSnackbarState(SUCCESS, successToModifyMsg));
+          setSnackbarState(getSnackbarState(SUCCESS, SNACKBAR_MSG.SUCCESS.MODIFY));
           setDialogOpen(false);
         },
       };
@@ -112,7 +122,7 @@ export const getDialogData = (
           dispatch(
             setCustomCategory({ categories: customCategory.filter((_, index) => idx !== index) }),
           );
-          setSnackbarState(getSnackbarState(SUCCESS, successToDeleteMsg));
+          setSnackbarState(getSnackbarState(SUCCESS, SNACKBAR_MSG.SUCCESS.DELETE));
           setDialogOpen(false);
         },
       };
