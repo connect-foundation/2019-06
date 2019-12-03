632b610bc9f116b807743176bf23dce94055596a
Jin <dkwk1254@naver.com>
2019-12-03 20:53:07 +0900

    feat(infra/maildb): 수신한 메일의 첨부파일을 object storage에 올리는 코드 추가
    attachment DB에 데이터 추가는 순서가 상관이 없으므로 for await -> Promise.all 변경
    log 객체를 삭제하고 log에 추가하던 값들을 mail객체에 추가
    ncloud.js에서 multipartUpload를 이용해 url값 반환

--- infra/maildb/src/app.js
+++ infra/maildb/src/app.js
@@ -1,19 +1,18 @@
 const simpleParser = require("mailparser").simpleParser;
 const mysql = require("mysql2/promise");
 const format = require("./format");
+const { multipartUpload } = require("./ncloud");
 
-const [
-  ORIGIN,
-  FILE_NAME,
-  MAIL_CONTENT,
+const {
   DB_DEV_USERNAME,
   DB_DEV_PW,
   DB_DEV_SCHEMA,
   DB_DEV_HOST
-] = process.argv;
+} = require("./env");
 
+const MAIL_CONTENT = process.argv[2];
 const MY_DOMAIN = "daitnu.com";
-const RECEIVED_KEY = 'received';
+const RECEIVED_KEY = "received";
 const UNTITLE = "";
 const EXP_EXTRACT_RECEIVER = /<.{3,40}@.{3,40}>/;
 
@@ -26,9 +25,15 @@ const pool = mysql.createPool({
 
 const parseMailContent = async content => {
   try {
-    const { from, to, subject, html, text, attachments, headers } = await simpleParser(
-      content
-    );
+    const {
+      from,
+      to,
+      subject,
+      html,
+      text,
+      attachments,
+      headers
+    } = await simpleParser(content);
     return {
       from: from.text,
       to: to.text,
@@ -48,7 +53,7 @@ const addReceiver = (receviers, email) => {
   if (domain === MY_DOMAIN) {
     receviers.push(id);
   }
-}
+};
 
 const getReceivers = ({ headers, to }) => {
   const receivedOfHeader = headers.get(RECEIVED_KEY)[0];
@@ -62,12 +67,15 @@ const getReceivers = ({ headers, to }) => {
   return receivers;
 };
 
-const recordLog = (mail, log) => {
+const recordLog = mail => {
+  const { from, to, mail_template_id, owner, category_no } = mail;
   console.log(
     JSON.stringify({
-      from: mail.from,
-      to: mail.to,
-      ...log
+      from,
+      to,
+      mail_template_id,
+      owner,
+      category_no
     })
   );
 };
@@ -77,30 +85,33 @@ const insertMailToDB = async content => {
   const receivers = getReceivers(mail);
   const connection = await pool.getConnection(async conn => conn);
   let queryFormat;
-  const log = {};
 
   try {
     await connection.beginTransaction();
     queryFormat = format.getQueryToAddMailTemplate(mail);
     const [{ insertId }] = await connection.execute(queryFormat);
 
-    for await (let attachment of mail.attachments) {
+    const insertingAttachments = mail.attachments.map(async attachment => {
+      const result = await multipartUpload(attachment);
+      attachment.url = result.key;
       attachment.mail_template_id = insertId;
       queryFormat = format.getQueryToAddAttachment(attachment);
-      await connection.execute(queryFormat);
-    }
+      return connection.execute(queryFormat);
+    });
 
-    for await (let id of receivers) {
+    const insertingMails = receivers.map(async id => {
       queryFormat = format.getQueryToFindOwnerAndCategoryNo(id);
       const [[{ owner, no }]] = await connection.query(queryFormat);
-      log.mail_template_id = insertId;
-      log.owner = owner;
-      log.category_no = no;
-      queryFormat = format.getQueryToAddMail(log);
-      await connection.execute(queryFormat);
-      recordLog(mail, log);
-    }
+      mail.mail_template_id = insertId;
+      mail.owner = owner;
+      mail.category_no = no;
+      queryFormat = format.getQueryToAddMail(mail);
+      recordLog(mail);
+      return connection.execute(queryFormat);
+    });
 
+    await Promise.all(insertingAttachments);
+    await Promise.all(insertingMails);
     await connection.commit();
     connection.release();
   } catch (err) {
