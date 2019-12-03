7d5d28c3e73348c8c4c9e9129aa9c0a8ab03d4ad
sjh2428 <pok_gare@naver.com>
2019-12-03 14:00:52 +0900

    fix(fe): 재사용 가능한 컴포넌트로 구조 변환

--- web/components/WriteMail/index.js
+++ web/components/WriteMail/index.js
@@ -4,23 +4,26 @@ import InputReceiver from './InputReceiver';
 import InputSubject from './InputSubject';
 import InputBody from './InputBody';
 import SubmitButton from './SubmitButton';
-import { WriteMailContextProvider } from './ContextProvider';
+import { WriteMailContextProvider, useDispatchForWM, useStateForWM } from './ContextProvider';
 import DropZone from './DropZone';
 import ReservationDateText from './ReservationDateText';
 
-const WriteMail = () => (
-  <WriteMailContextProvider>
-    <S.WriteArea>
-      <InputReceiver />
-      <InputSubject />
-      <InputBody />
-      <S.RowContainer>
-        <SubmitButton />
-        <ReservationDateText />
-      </S.RowContainer>
-      <DropZone />
-    </S.WriteArea>
-  </WriteMailContextProvider>
-);
+const WriteMail = () => {
+  const props = { useStateForWM, useDispatchForWM };
+  return (
+    <WriteMailContextProvider>
+      <S.WriteArea>
+        <InputReceiver {...props} />
+        <InputSubject {...props} />
+        <InputBody {...props} />
+        <S.RowContainer>
+          <SubmitButton {...props} />
+          <ReservationDateText {...props} />
+        </S.RowContainer>
+        <DropZone {...props} />
+      </S.WriteArea>
+    </WriteMailContextProvider>
+  );
+};
 
 export default WriteMail;
