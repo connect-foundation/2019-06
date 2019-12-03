6e90dc3c265eb6c03c1a16d83a3bed9cf0897fc3
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-03 17:31:42 +0900

    fix(be/attachment): 첨부파일 size 추가

--- server/src/v1/mail/service.js
+++ server/src/v1/mail/service.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-return-await */
 /* eslint-disable import/prefer-default-export */
 import nodemailer from 'nodemailer';
+import { Op } from 'sequelize';
 import DB from '../../database/index';
 import U from '../../libraries/mail-util';
 import getPaging from '../../libraries/paging';
@@ -9,7 +10,6 @@ import { saveSentMail } from '../../libraries/save-to-infra';
 import ERROR_CODE from '../../libraries/exception/error-code';
 import ErrorResponse from '../../libraries/exception/error-response';
 import ErrorField from '../../libraries/exception/error-field';
-import { Op } from 'sequelize';
 
 const SENT_MAILBOX_NAME = '보낸메일함';
 const WASTEBASKET_NAME = '휴지통';
