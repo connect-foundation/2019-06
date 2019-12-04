be737917d9bd6d49e2ede00c9475a3d665e90642
sjh2428 <pok_gare@naver.com>
2019-12-04 16:22:11 +0900

    fix(fe): 첨부파일 이름 hover 스타일 수정

--- web/components/ReadMail/FileList/File/styled.js
+++ web/components/ReadMail/FileList/File/styled.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 const FileSpan = styled.span`
   cursor: pointer;
-  &::hover {
+  &:hover {
+    font-weight: bold;
     text-decoration: underline;
   }
 `;
