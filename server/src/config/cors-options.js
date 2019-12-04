c045cc65f5cdee2ac665e549f3c343df874042bf
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-04 20:04:51 +0900

    feat(be/cors): 허용된 메서드 제한

--- server/src/config/cors-options.js
+++ server/src/config/cors-options.js
@@ -1,7 +1,20 @@
+import ErrorResponse from '../libraries/exception/error-response';
+import ERROR_CODE from '../libraries/exception/error-code';
+
+const corsErrorResponse = new ErrorResponse(ERROR_CODE.NOT_ALLOWED_BY_CORS);
 const { FRONTEND_SERVER_ADDRESS } = process.env;
 
+const whitelist = {
+  [FRONTEND_SERVER_ADDRESS]: true,
+  'https://daitnu.com': true,
+  'https://www.daitnu.com': true,
+};
+
 const corsOptions = {
-  origin: FRONTEND_SERVER_ADDRESS,
+  origin(origin, callback) {
+    return whitelist[origin] || !origin ? callback(null, true) : callback(corsErrorResponse);
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 };
 
--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -2,7 +2,6 @@ import ErrorResponse from '../libraries/exception/error-response';
 import ErrorCode from '../libraries/exception/error-code';
 
 const isAuth = (req, res, next) => {
-  // if (!req.isAuthenticated()) {
   if (!req.user) {
     return next(new ErrorResponse(ErrorCode.UNAUTHORIZED));
   }
