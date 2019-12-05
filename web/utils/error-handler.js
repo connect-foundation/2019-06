8172145bdc14a4bce69347289798e0633e864568
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-05 17:42:13 +0900

    fix(fe/error): 에러핸들러 status에따른 컴포넌트 연동

--- web/utils/error-handler.js
+++ web/utils/error-handler.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Router from 'next/router';
+import UserError from '../components/Error/User';
+import ServerError from '../components/Error/Server';
 
 const handleErrorStatus = ({ status, message }) => {
   let returnValue = '';
-  switch (status) {
-    case 401:
-      Router.push('/login');
-      break;
-    default:
-      returnValue = <div>{message}</div>;
-      break;
+
+  if (status === 401) {
+    Router.push('/login');
+  } else if (status >= 400 && status < 500) {
+    returnValue = <UserError message={message} />;
+  } else if (status >= 500) {
+    returnValue = <ServerError message={message} />;
   }
+
   return returnValue;
 };
 
