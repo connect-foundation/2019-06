c67dbd9553b7a62b6dbfe17e4a4460b4a6a30334
hwanlee <ljhw3377@gmail.com>
2019-12-07 17:47:04 +0900

    fix(be): 회원가입시 아이디 혹은 이메일 중복 처리

--- server/src/v1/users/service.js
+++ server/src/v1/users/service.js
@@ -2,6 +2,7 @@ import generator from 'generate-password';
 
 import DB from '../../database';
 import ErrorResponse from '../../libraries/exception/error-response';
+import ErrorField from '../../libraries/exception/error-field';
 import ERROR_CODE from '../../libraries/exception/error-code';
 import mailUtil from '../../libraries/mail-util';
 import { encrypt, aesEncrypt } from '../../libraries/crypto';
@@ -9,14 +10,39 @@ import { encrypt, aesEncrypt } from '../../libraries/crypto';
 const DEFAULT_CATEGORIES = ['받은메일함', '보낸메일함', '내게쓴메일함', '휴지통'];
 const TEMP_PASSWORD_LENGTH = 15;
 
-const isSubEmailUniqueConstraintError = error => {
+const checkSubEmailUniqueConstraintError = error => {
   if (error.errors) {
-    const [{ type, path }] = error.errors;
+    const [{ type, path, value }] = error.errors;
     if (type === 'unique violation' && path === 'sub_email') {
-      return true;
+      const errorField = new ErrorField('sub_email', value, '이미 가입에 사용한 이메일 입니다.');
+      throw new ErrorResponse(ERROR_CODE.ID_OR_SUB_EMAIL_DUPLICATION, errorField);
     }
   }
-  return false;
+  throw error;
+};
+
+const makeUniqueFieldsError = (response, userData) => {
+  const errorFields = [];
+  let errorField;
+
+  if (response.id === userData.id) {
+    errorField = new ErrorField('id', userData.id, '이미 사용중인 아이디 입니다.');
+    errorFields.push(errorField);
+  }
+  if (response.sub_email === userData.sub_email) {
+    errorField = new ErrorField(
+      'sub_email',
+      userData.sub_email,
+      '이미 가입에 사용한 이메일 입니다.',
+    );
+    errorFields.push(errorField);
+  }
+
+  if (errorFields.length > 0) {
+    throw new ErrorResponse(ERROR_CODE.ID_OR_SUB_EMAIL_DUPLICATION, errorFields);
+  }
+
+  return true;
 };
 
 const createDefaultCategories = async (no, transaction) => {
@@ -37,16 +63,13 @@ const register = async ({ id, password, name, sub_email }) => {
     await DB.sequelize.transaction(async transaction => {
       const [response, created] = await DB.User.findOrCreateById(userData, { transaction });
       if (!created) {
-        throw new ErrorResponse(ERROR_CODE.ID_DUPLICATION);
+        throw makeUniqueFieldsError(response, userData);
       }
       newUser = response.get({ plain: true });
       await createDefaultCategories(newUser.no, transaction);
     });
   } catch (error) {
-    if (isSubEmailUniqueConstraintError(error)) {
-      throw new ErrorResponse(ERROR_CODE.SUB_EMAIL_DUPLICATION);
-    }
-    throw error;
+    checkSubEmailUniqueConstraintError(error);
   }
 
   return newUser;
