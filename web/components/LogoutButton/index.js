6537682e9ea1b4168bed9fe647a2649e5f3dc6f7
hwanlee <ljhw3377@gmail.com>
2019-12-08 21:50:29 +0900

    fix(fe): 기존 LogoutButton 스타일 변경

--- web/components/LogoutButton/index.js
+++ web/components/LogoutButton/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 import request from '../../utils/request';
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    margin: theme.spacing(1),
-    width: '100px',
-  },
-}));
-
 export default () => {
-  const classes = useStyles();
-
   const handleSignOutBtnClick = () => {
     window.sessionStorage.clear();
     request.post('/auth/logout', {});
@@ -21,7 +11,7 @@ export default () => {
   };
 
   return (
-    <Button variant="contained" className={classes.button} onClick={handleSignOutBtnClick}>
+    <Button variant="contained" onClick={handleSignOutBtnClick}>
       로그아웃
     </Button>
   );
