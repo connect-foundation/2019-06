8a3126e515a8515cb7b8ce15aec3aac8c249f869
Jin <dkwk1254@naver.com>
2019-12-09 10:46:52 +0900

    feat(fe/page-move-button-area): 이전/다음 메일 보기 구현
    paging하여 메일리스트를 context에 저장한 mails를 이용하여 이전/다음 메일 출력
    page가 바뀌는 부분에서만 서버로부터 리스트를 받아 전역 context의 mails에 저장

--- web/components/ReadMail/PageMoveButtonArea/index.js
+++ web/components/ReadMail/PageMoveButtonArea/index.js
@@ -1,31 +1,62 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import { ArrowBack, ArrowForward } from '@material-ui/icons';
 import * as S from './styled';
 import request from '../../../utils/request';
 import { AppStateContext, AppDispatchContext } from '../../../contexts';
-import { handleMailClick } from '../../../contexts/reducer';
+import { setMail, handleMailsChange } from '../../../contexts/reducer';
 import getQueryByOptions from '../../../utils/query';
-import ReadMail from '..';
+
+const loadNewMails = async ({ query, dispatch }) => {
+  const { isError, data } = await request.get(`/mail/?${query}`);
+  if (!isError) {
+    dispatch(handleMailsChange({ ...data }));
+  }
+  return data.mails;
+};
+
+const isDisabledPrevBtn = (index, paging) => index < 1 && paging.page === 1;
+const isDisabledNextBtn = (index, paging, mailCount) =>
+  index === mailCount - 1 && paging.page === paging.totalPage;
 
 const PageMoveButtonArea = ({ index }) => {
   const { state } = useContext(AppStateContext);
   const { dispatch } = useContext(AppDispatchContext);
-  const { mails, category, page, sort } = state;
+  const { mails, category, sort, paging, page } = state;
+  const mailCount = mails.length;
 
-  const handleArrowClick = indexOfMails => {
-    mails[indexOfMails].index = indexOfMails;
-    dispatch(handleMailClick(mails[indexOfMails], <ReadMail />));
+  const handlePrevBtnClick = async () => {
+    let newMails = mails;
+    if (index === 0) {
+      const query = getQueryByOptions({ category, page: page - 1, sort });
+      newMails = await loadNewMails({ query, dispatch });
+      index = mails.length;
+    }
+    const mail = newMails[index - 1];
+    mail.index = index - 1;
+    dispatch(setMail(mail));
+  };
+
+  const handleNextBtnClick = async () => {
+    let newMails = mails;
+    if (index === mails.length - 1) {
+      const query = getQueryByOptions({ category, page: page + 1, sort });
+      newMails = await loadNewMails({ query, dispatch });
+      index = -1;
+    }
+    const mail = newMails[index + 1];
+    mail.index = index + 1;
+    dispatch(setMail(mail));
   };
 
   return (
     <S.Container>
-      <IconButton disabled={index < 1} onClick={handleArrowClick.bind(null, index - 1)}>
+      <IconButton disabled={isDisabledPrevBtn(index, paging)} onClick={handlePrevBtnClick}>
         <ArrowBack />
       </IconButton>
       <IconButton
-        disabled={index === mails.length - 1}
-        onClick={handleArrowClick.bind(null, index + 1)}>
+        disabled={isDisabledNextBtn(index, paging, mailCount)}
+        onClick={handleNextBtnClick}>
         <ArrowForward />
       </IconButton>
     </S.Container>
