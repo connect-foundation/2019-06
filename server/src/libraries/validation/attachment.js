40d3ba3f855834317b5ae4da23d1201603e118c0
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-09 15:07:50 +0900

    fix(be/attachment): 파일 확장자 검사 블랙리스트로 변경

--- server/src/libraries/validation/attachment.js
+++ server/src/libraries/validation/attachment.js
@@ -2,30 +2,78 @@ import ERROR_CODE from '../exception/error-code';
 import ErrorField from '../exception/error-field';
 import ErrorResponse from '../exception/error-response';
 
-const MB = 1024 * 1024;
-const FILE_MAX_SIZE = 9 * MB;
+const MB = 1000 * 1000;
+const FILE_MAX_SIZE = 10 * MB;
 
-const AVAILABLE_MIME_TYPE = {
-  'text/plain': true,
-  'text/csv': true,
-  'text/html': true,
-  'application/vnd.ms-powerpoint': true,
-  'application/haansoftxlsx': true,
-  'application/pdf': true,
-  'application/x-font-ttf': true,
-  'application/zip': true,
-  'application/json': true,
-  'application/haansoftpptx': true,
-  'application/vnd.ms-excel': true,
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document': true,
-  'application/vnd.openxmlformats-officedocument.presentationml.presentation': true,
-  'application/msword': true,
-  'image/jpeg': true,
-  'image/png': true,
-  'image/jpg': true,
-  'image/bmp': true,
-  'image/gif': true,
-  '': true,
+const UNAVAILABLE_EXTENSION = {
+  bat: true,
+  cmd: true,
+  com: true,
+  cpl: true,
+  exe: true,
+  js: true,
+  scr: true,
+  vds: true,
+  wsf: true,
+  jse: true,
+  adp: true,
+  chm: true,
+  hta: true,
+  lnk: true,
+  mde: true,
+  msc: true,
+  msi: true,
+  msp: true,
+  mst: true,
+  pif: true,
+  sct: true,
+  shb: true,
+  vb: true,
+  vbe: true,
+  wsc: true,
+  wsh: true,
+  ade: true,
+  jar: true,
+  bas: true,
+  cer: true,
+  crt: true,
+  der: true,
+  gadget: true,
+  hlp: true,
+  inf: true,
+  mad: true,
+  maf: true,
+  mag: true,
+  mam: true,
+  maq: true,
+  mar: true,
+  mas: true,
+  mat: true,
+  mau: true,
+  mav: true,
+  maw: true,
+  mda: true,
+  mdb: true,
+  mdt: true,
+  mdw: true,
+  mdz: true,
+  reg: true,
+  scf: true,
+  shs: true,
+  ps1: true,
+  ps1xml: true,
+  ps2: true,
+  ps2xml: true,
+  psc1: true,
+  psc2: true,
+  url: true,
+  grp: true,
+  xbap: true,
+  ocx: true,
+  nsh: true,
+  sys: true,
+  vxd: true,
+  jnlp: true,
 };
 
 const add = (a, b) => a + b;
@@ -35,11 +83,27 @@ const getTotalSize = sizes => {
   return sum;
 };
 
-const checkMimeType = mimetypes => mimetypes.every(mimetype => AVAILABLE_MIME_TYPE[mimetype]);
+const checkExtension = fileNames => {
+  const errors = [];
+  for (const fileName of fileNames) {
+    const splitedFileName = fileName.split('.');
+    let extension = splitedFileName[splitedFileName.length - 1];
+    extension = extension.toLowerCase();
+
+    if (UNAVAILABLE_EXTENSION[extension]) {
+      errors.push(fileName);
+    }
+  }
+
+  if (errors.length > 0) {
+    throw errors.join('  ');
+  }
+  return true;
+};
 
 const checkAttachment = files => {
   const sizes = files.map(file => file.size);
-  const mimetypes = files.map(file => file.mimetype);
+  const fileNames = files.map(file => file.originalname);
   const errorFields = [];
 
   const totalSize = getTotalSize(sizes);
@@ -48,11 +112,13 @@ const checkAttachment = files => {
     errorFields.push(errorField);
   }
 
-  if (!checkMimeType(mimetypes)) {
+  try {
+    checkExtension(fileNames);
+  } catch (error) {
     const errorField = new ErrorField(
-      'mimetype',
-      mimetypes,
-      '허용되지 않는 mimetype이 포함되어 있습니다.',
+      'file extension',
+      error,
+      '허용되지 않는 확장자가 포함되어 있습니다.',
     );
     errorFields.push(errorField);
   }
@@ -64,4 +130,4 @@ const checkAttachment = files => {
   return true;
 };
 
-export { checkAttachment, checkMimeType, getTotalSize };
+export { checkAttachment, checkExtension, getTotalSize };
