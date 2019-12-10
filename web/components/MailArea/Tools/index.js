f296a8cd59aac1d1780881c56f998810e9a1e70f
Jin <dkwk1254@naver.com>
2019-12-10 17:42:02 +0900

    feat(fe/read-mail/tools): 체크박스에 의한 메일 삭제
    체크가 된 mail들은 selected값이 true이므로 이것을 이용하여 category_no를 업데이트하여 휴지통으로 이동될 수 있도록 구현
    삭제가 성공하면 몇 개가 삭제되었는지 스낵바를 통해 출력

--- web/components/MailArea/Tools/index.js
+++ web/components/MailArea/Tools/index.js
@@ -13,9 +13,27 @@ import {
 import { ArrowDownward, ArrowUpward, Email, Send, Delete, DeleteForever } from '@material-ui/icons';
 import S from './styled';
 import { AppDispatchContext, AppStateContext } from '../../../contexts';
-import { handleSortSelect, handleCheckAllMails } from '../../../contexts/reducer';
+import {
+  handleSortSelect,
+  handleCheckAllMails,
+  handleSnackbarState,
+  handleMailsChange,
+  initCheckerInTools,
+} from '../../../contexts/reducer';
+import getQueryByOptions from '../../../utils/query';
+import request from '../../../utils/request';
+import { getSnackbarState, SNACKBAR_VARIANT } from '../../Snackbar';
 
 const WASTEBASKET_NAME = '휴지통';
+
+const SNACKBAR_MSG = {
+  ERROR: {
+    DELETE: '메일 삭제를 실패하였습니다.',
+  },
+  SUCCESS: {
+    DELETE: count => `${count}개의 메일을 삭제하였습니다.`,
+  },
+};
 
 const useStyles = makeStyles(theme => ({
   formControl: {
@@ -43,6 +61,18 @@ const getArrowIcon = sortValue =>
     <ArrowDownward fontSize={'small'} />
   );
 
+const loadNewMails = async (query, dispatch) => {
+  const { isError, data } = await request.get(`/mail/?${query}`);
+  if (isError) {
+    throw SNACKBAR_MSG.ERROR.LOAD;
+  }
+  dispatch(handleMailsChange({ ...data }));
+};
+
+const updateMails = async (nos, props) => {
+  return request.patch(`/mail`, { nos, props });
+};
+
 const sortItems = SORT_TYPES.map(type => (
   <MenuItem key={type.value} value={type.value}>
     <S.SortItemView>
@@ -58,24 +88,45 @@ const buttons = [
     name: '답장',
     enable: true,
     icon: <Email />,
+    onClick: () => {},
   },
   {
     key: 'send',
     name: '전달',
     enable: true,
     icon: <Send />,
+    onClick: () => {},
   },
   {
     key: 'delete',
     name: '삭제',
     enable: true,
     icon: <Delete />,
+    onClick: async ({ mails, dispatch, query, wastebasketNo, openSnackbar }) => {
+      try {
+        const nos = mails.filter(({ selected }) => selected).map(({ no }) => no);
+        const deletedCount = nos.length;
+        if (deletedCount === 0) {
+          return;
+        }
+        const { isError } = await updateMails(nos, { category_no: wastebasketNo });
+        if (isError) {
+          throw SNACKBAR_MSG.ERROR.DELETE;
+        }
+        await loadNewMails(query, dispatch);
+        openSnackbar(SNACKBAR_VARIANT.SUCCESS, SNACKBAR_MSG.SUCCESS.DELETE(deletedCount));
+      } catch (errorMessage) {
+        openSnackbar(SNACKBAR_VARIANT.ERROR, errorMessage);
+      } finally {
+        dispatch(initCheckerInTools());
+      }
+    },
   },
   {
     key: 'forever_delete',
     name: '영구삭제',
-    icon: <DeleteForever />,
     enable: true,
+    icon: <DeleteForever />,
     onClick: () => {},
   },
 ];
@@ -88,7 +139,10 @@ const Tools = () => {
   const { state } = useContext(AppStateContext);
   const { dispatch } = useContext(AppDispatchContext);
   const { allMailCheckInTools, mails, category, categoryNoByName } = state;
+  const query = getQueryByOptions(state);
   const wastebasketNo = categoryNoByName[WASTEBASKET_NAME];
+  const openSnackbar = (variant, message) =>
+    dispatch(handleSnackbarState(getSnackbarState(variant, message)));
   const handleFilterChange = ({ target: { value } }) => dispatch(handleSortSelect(value));
   const handleCheckAllChange = () => dispatch(handleCheckAllMails(allMailCheckInTools, mails));
 
@@ -108,6 +162,7 @@ const Tools = () => {
           color="primary"
           className={classes.button}
           startIcon={btn.icon}
+          onClick={btn.onClick.bind(null, { mails, dispatch, query, openSnackbar, wastebasketNo })}
           key={btn.key}>
           {btn.name}
         </Button>
