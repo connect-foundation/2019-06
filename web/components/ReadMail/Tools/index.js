3c3bbd587b42b223e5bd4f81e9498a3c164ea56a
Jin <dkwk1254@naver.com>
2019-12-10 22:52:01 +0900

    feat(fe/read-mail/tools): 휴지통의 메일읽기 화면에서 메일 복구 구현
    삭제할 때 이전 카테고리를 저장해놓은 prev_category_no를 이용하여 복구
    복구가 성공하면 해당 카테고리로 이동

--- web/components/ReadMail/Tools/index.js
+++ web/components/ReadMail/Tools/index.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
 import { Email, Send, Delete, DeleteForever } from '@material-ui/icons';
 import { AppDispatchContext, AppStateContext } from '../../../contexts';
-import { handleSnackbarState, setView } from '../../../contexts/reducer';
+import { handleSnackbarState, setView, handleCategoryClick } from '../../../contexts/reducer';
 import { getSnackbarState, SNACKBAR_VARIANT } from '../../Snackbar';
 import S from './styled';
 import MailArea from '../../MailArea';
@@ -13,10 +13,12 @@ const WASTEBASKET_NAME = '휴지통';
 const SNACKBAR_MSG = {
   ERROR: {
     DELETE: '메일 삭제에 실패하였습니다.',
+    RECYLCE: '메일 복구에 실패하였습니다.',
     FOREVER_DELETE: '메일 영구 삭제에 실패하였습니다.',
   },
   SUCCESS: {
     DELETE: '메일을 삭제하였습니다.',
+    RECYLCE: '메일을 복구하였습니다.',
     FOREVER_DELETE: '메일을 영구 삭제하였습니다.',
   },
 };
@@ -51,7 +53,7 @@ const buttons = [
     name: '삭제',
     enable: true,
     icon: <Delete />,
-    onClick: async ({ mail, openSnackbar, wastebasketNo, backToMailArea }) => {
+    onClick: async ({ mail, openSnackbar, wastebasketNo, dispatch }) => {
       const props = { category_no: wastebasketNo };
       const { isError } = await request.patch(`/mail/${mail.no}`, { props });
       if (isError) {
@@ -59,7 +61,23 @@ const buttons = [
         return;
       }
       openSnackbar(SNACKBAR_VARIANT.SUCCESS, SNACKBAR_MSG.SUCCESS.DELETE);
-      backToMailArea();
+      dispatch(setView(<MailArea />));
+    },
+  },
+  {
+    key: 'recycle',
+    name: '복구',
+    enable: true,
+    icon: <Delete />,
+    onClick: async ({ mail, openSnackbar, dispatch }) => {
+      const props = { category_no: mail.prev_category_no };
+      const { isError } = await request.patch(`/mail/${mail.no}`, { props });
+      if (isError) {
+        openSnackbar(SNACKBAR_VARIANT.ERROR, SNACKBAR_MSG.ERROR.RECYLCE);
+        return;
+      }
+      openSnackbar(SNACKBAR_VARIANT.SUCCESS, SNACKBAR_MSG.SUCCESS.RECYLCE);
+      dispatch(handleCategoryClick(mail.prev_category_no, <MailArea />));
     },
   },
   {
@@ -67,20 +85,21 @@ const buttons = [
     name: '영구삭제',
     icon: <DeleteForever />,
     enable: true,
-    onClick: async ({ mail, openSnackbar, backToMailArea }) => {
+    onClick: async ({ mail, openSnackbar, dispatch }) => {
       const { isError } = await request.delete(`/mail/${mail.no}`);
       if (isError) {
         openSnackbar(SNACKBAR_VARIANT.ERROR, SNACKBAR_MSG.ERROR.FOREVER_DELETE);
         return;
       }
       openSnackbar(SNACKBAR_VARIANT.SUCCESS, SNACKBAR_MSG.SUCCESS.FOREVER_DELETE);
-      backToMailArea();
+      dispatch(setView(<MailArea />));
     },
   },
 ];
 
 const deleteButton = buttons.find(button => button.key === 'delete');
 const foreverDeleteButton = buttons.find(button => button.key === 'forever_delete');
+const recylceButton = buttons.find(button => button.key === 'recycle');
 
 const Tools = () => {
   const { state } = useContext(AppStateContext);
@@ -90,14 +109,15 @@ const Tools = () => {
   const wastebasketNo = categoryNoByName[WASTEBASKET_NAME];
   const openSnackbar = (variant, message) =>
     dispatch(handleSnackbarState(getSnackbarState(variant, message)));
-  const backToMailArea = () => dispatch(setView(<MailArea />));
 
   if (mail.category_no === wastebasketNo) {
     deleteButton.enable = false;
     foreverDeleteButton.enable = true;
+    recylceButton.enable = true;
   } else {
     deleteButton.enable = true;
     foreverDeleteButton.enable = false;
+    recylceButton.enable = false;
   }
 
   const buttonSet = buttons.map(btn => {
@@ -108,7 +128,7 @@ const Tools = () => {
           color="primary"
           className={classes.button}
           startIcon={btn.icon}
-          onClick={btn.onClick.bind(null, { mail, openSnackbar, wastebasketNo, backToMailArea })}
+          onClick={btn.onClick.bind(null, { mail, openSnackbar, wastebasketNo, dispatch })}
           key={btn.key}>
           {btn.name}
         </Button>
