ea659adfe24056307746728fc75d14265d6de768
sjh2428 <pok_gare@naver.com>
2019-12-10 16:13:40 +0900

    feat(infra): 인프라 작업 동기화
    - 형진님 코드 합병
    - advanced content filter 파일 인자를 전달하는 방식으로 수정
    - 인프라에서 보낸 메일 DB에 저장하는 방식으로 수정

--- infra/maildb/src/app.js
+++ infra/maildb/src/app.js
@@ -1,19 +1,24 @@
 const simpleParser = require("mailparser").simpleParser;
 const mysql = require("mysql2/promise");
 const format = require("./format");
+const { multipartUpload } = require("./ncloud");
+const fs = require("fs");
 
-const [
-  ORIGIN,
-  FILE_NAME,
-  MAIL_CONTENT,
+const {
   DB_DEV_USERNAME,
   DB_DEV_PW,
   DB_DEV_SCHEMA,
   DB_DEV_HOST
-] = process.argv;
+} = require("./env");
 
+const MAIL_CONTENT = process.argv[2];
 const MY_DOMAIN = "daitnu.com";
-const UNTITLE = "";
+const RECEIVED_KEY = "received";
+const MESSAGE_ID_KEY = "message-id";
+const NOTITLE = "제목없음";
+const NOTEXT = "";
+const EXP_EXTRACT_RECEIVER = /<.{3,40}@.{3,40}>/;
+const EXP_EXTRACT_SENDER = /.{3,40}@.{3,40}/;
 
 const pool = mysql.createPool({
   user: DB_DEV_USERNAME,
@@ -24,15 +29,23 @@ const pool = mysql.createPool({
 
 const parseMailContent = async content => {
   try {
-    const { from, to, subject, html, text, attachments } = await simpleParser(
-      content
-    );
+    const {
+      from,
+      to,
+      subject,
+      html,
+      text,
+      attachments,
+      headers
+    } = await simpleParser(content);
     return {
       from: from.text,
       to: to.text,
-      subject: subject || UNTITLE,
-      text: html || text,
-      attachments
+      subject: subject || NOTITLE,
+      html,
+      text: text || NOTEXT,
+      attachments,
+      headers
     };
   } catch (err) {
     console.log(err);
@@ -40,56 +53,100 @@ const parseMailContent = async content => {
   }
 };
 
-const getReceivers = to => {
+const addReceiver = (receviers, email) => {
+  const [id, domain] = email.trim().split("@");
+  if (domain === MY_DOMAIN) {
+    receviers.push(id);
+  }
+};
+
+const getReceivers = ({ headers, to }) => {
+  const receivedOfHeader = headers.get(RECEIVED_KEY)[0];
+  const realReceiver = EXP_EXTRACT_RECEIVER.exec(receivedOfHeader);
   const receivers = [];
-  to.split(",").forEach(email => {
-    const [id, domain] = email.split("@");
-    if (domain === MY_DOMAIN) {
-      receivers.push(id);
-    }
-  });
+  if (realReceiver) {
+    addReceiver(receivers, realReceiver[0].slice(1, -1));
+  } else {
+    to.split(",").forEach(email => addReceiver(receivers, email));
+  }
   return receivers;
 };
 
-const recordLog = (mail, log) => {
+const recordLog = mail => {
+  const { from, to, mail_template_id, owner, category_no, message_id } = mail;
   console.log(
     JSON.stringify({
-      from: mail.from,
-      to: mail.to,
-      ...log
+      from,
+      to,
+      mail_template_id,
+      owner,
+      category_no,
+      message_id
     })
   );
 };
 
+const setMessageIdOfMail = mail => {
+  const { headers } = mail;
+  const messageId = headers.get(MESSAGE_ID_KEY);
+  mail.message_id = messageId.slice(1, -1);
+};
+
+const getSender = from => {
+  let senderId, senderDomain;
+  let sender = EXP_EXTRACT_RECEIVER.exec(from);
+  if (!sender) {
+    [senderId, senderDomain] = from.split("@");
+  } else {
+    [senderId, senderDomain] = sender[0].slice(1, -1).split("@");
+  }
+  return [senderId, senderDomain];
+};
+
 const insertMailToDB = async content => {
   const mail = await parseMailContent(content);
-  const receivers = getReceivers(mail.to);
+  const receivers = getReceivers(mail);
+  const [senderId, senderDomain] = getSender(mail.from);
+  setMessageIdOfMail(mail);
   const connection = await pool.getConnection(async conn => conn);
   let queryFormat;
-  const log = {};
 
   try {
     await connection.beginTransaction();
     queryFormat = format.getQueryToAddMailTemplate(mail);
     const [{ insertId }] = await connection.execute(queryFormat);
 
-    for await (let attachment of mail.attachments) {
+    const insertingAttachments = mail.attachments.map(async attachment => {
+      const result = await multipartUpload(attachment);
+      attachment.url = result.key;
       attachment.mail_template_id = insertId;
       queryFormat = format.getQueryToAddAttachment(attachment);
-      await connection.execute(queryFormat);
-    }
+      return connection.execute(queryFormat);
+    });
 
-    for await (let id of receivers) {
+    const insertingMails = receivers.map(async id => {
       queryFormat = format.getQueryToFindOwnerAndCategoryNo(id);
       const [[{ owner, no }]] = await connection.query(queryFormat);
-      log.mail_template_id = insertId;
-      log.owner = owner;
-      log.category_no = no;
-      queryFormat = format.getQueryToAddMail(log);
-      await connection.execute(queryFormat);
-      recordLog(mail, log);
+      mail.mail_template_id = insertId;
+      mail.owner = owner;
+      mail.category_no = no;
+      queryFormat = format.getQueryToAddMail(mail);
+      return connection.execute(queryFormat);
+    });
+
+    if (senderDomain === MY_DOMAIN) {
+      // sender가 daitnu면 sender id의 보낸메일함에 DB 저장
+      queryFormat = format.getQueryToFindOwnerAndCategoryNo(senderId, true);
+      const [[{ owner, no }]] = await connection.query(queryFormat);
+      mail.mail_template_id = insertId;
+      mail.owner = owner;
+      mail.category_no = no;
+      queryFormat = format.getQueryToAddMail(mail);
+      insertingMails.push(connection.execute(queryFormat));
     }
 
+    await Promise.all(insertingAttachments);
+    await Promise.all(insertingMails);
     await connection.commit();
     connection.release();
   } catch (err) {
@@ -99,6 +156,16 @@ const insertMailToDB = async content => {
   } finally {
     pool.end();
   }
+
+  return mail;
 };
 
-insertMailToDB(MAIL_CONTENT);
+fs.readFile(MAIL_CONTENT, "utf8", async (err, data) => {
+  if (err) {
+    throw err;
+  }
+  const mail = await insertMailToDB(data);
+  if (mail) {
+    recordLog(mail);
+  }
+});
