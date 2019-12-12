abfc338a6817586d53275cb172592dff6442679d
Jin <dkwk1254@naver.com>
2019-12-12 17:54:57 +0900

    feat(fe/write-mail): 답장의 경우 받는사람과 제목에 default 값 설정
    답장의 경우 WriteMail은 메일 정보를 받아서 받는사람과 제목을 default값으로 설정하도록 한다.

--- web/components/WriteMail/index.js
+++ web/components/WriteMail/index.js
@@ -9,15 +9,16 @@ import DropZone from './DropZone';
 
 const InputBody = dynamic(import('./InputBody'), { ssr: false });
 
-const WriteMail = () => {
+const WriteMail = ({ mailToReply }) => {
   const [dropZoneVisible, setDropZoneVisible] = useState(false);
+  const { from, subject } = mailToReply.MailTemplate;
 
   return (
     <WriteMailContextProvider>
       <S.WriteArea>
         <Tools dropZoneVisible={dropZoneVisible} setDropZoneVisible={setDropZoneVisible} />
-        <InputReceiver />
-        <InputSubject />
+        <InputReceiver defaultReceiver={from} />
+        <InputSubject defaultSubject={subject} />
         <DropZone visible={dropZoneVisible} />
         <InputBody />
       </S.WriteArea>
