4dd43f2a1b51e4237b0aa580e0f83b9ca611bf98
Jin <dkwk1254@naver.com>
2019-12-12 11:19:36 +0900

    chore(fe/utils/mail-request): convertNumberToArray -> convertToArray
    Number에 종속시키지 않으며 삼항연산자를 이용하여 단순화

--- web/utils/mail-request.js
+++ web/utils/mail-request.js
@@ -1,23 +1,18 @@
 import request from './request';
 
-const convertNumberToArray = no => {
-  if (!Array.isArray(no)) {
-    return [no];
-  }
-  return no;
-};
+const convertToArray = no => (Array.isArray(no) ? no : [no]);
 
 const update = async (no, props) => {
-  const nos = convertNumberToArray(no);
+  const nos = convertToArray(no);
   return request.patch('/mail', { nos, props });
 };
 
 const remove = async no => {
-  const nos = convertNumberToArray(no);
+  const nos = convertToArray(no);
   return request.delete('/mail', { nos });
 };
 
-const get = async url => {
+const get = url => {
   return request.get(url);
 };
 
