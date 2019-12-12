f4c87504561e4bf6010594460b6c0da6f02ebeab
sjh2428 <pok_gare@naver.com>
2019-12-12 16:15:42 +0900

    feat(be): node imap에서 message id를 통하여 검색을 하기위한 make args 함수 작성
    node imap에서 2개 이상의 메일을 검색하기 위해서는
    or 연산자를 배열에 중첩하여 사용하여야 했기 때문에
    메세지 아이디의 리스트만 주면 알맞게 변환해주는 함수가 필요하였음

--- server/src/libraries/save-to-infra.js
+++ server/src/libraries/save-to-infra.js
@@ -31,6 +31,37 @@ const connectImap = ({ email, password }, callback) => {
   imap.connect();
 };
 
+const makeArg = val => ['HEADER', 'MESSAGE-ID', val];
+
+const makeSearchArgs = array => {
+  if (!Array.isArray(array)) {
+    throw new Error('argument must be array');
+  }
+
+  let result = [],
+    cur,
+    prev;
+
+  if (array.length === 1) {
+    result.push(makeArg(array[0]));
+    return result;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    if (i === 0) {
+      result.push(makeArg(array[i]));
+    } else if (i === 1) {
+      result.unshift('OR', makeArg(array[i]));
+      prev = result;
+    } else {
+      cur = ['OR', prev[2], makeArg(array[i])];
+      prev[2] = cur;
+      prev = prev[2];
+    }
+  }
+  return [result];
+};
+
 export const saveToMailbox = ({ user, msg, mailboxName }) => {
   connectImap(user, imap => {
     imap.append(msg.toString(), { mailbox: PREFIX + mailboxName });
