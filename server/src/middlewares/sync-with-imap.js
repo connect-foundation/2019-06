52cb623df33f23674925ddb55ea8fb3308017199
sjh2428 <pok_gare@naver.com>
2019-12-15 16:38:28 +0900

    refactor(be): 데이터베이스에 저장된 메일 불러오는 부분 분리

--- server/src/middlewares/sync-with-imap.js
+++ server/src/middlewares/sync-with-imap.js
@@ -1,11 +1,7 @@
 import { getImapMessageIds } from '../libraries/imap';
 import DB from '../database';
 
-const syncWithImap = async (req, res, next) => {
-  const imapMessageIds = await getImapMessageIds({
-    user: { email: 'yaahoo@daitnu.com', password: '12345678' },
-  });
-  const categories = await DB.Category.findAllByUserNo(4);
+const getDbMails = async imapMessageIds => {
   const dbMails = {};
   const mailBoxNames = Object.keys(imapMessageIds);
   for (let i = 0; i < mailBoxNames.length; i++) {
@@ -20,10 +16,19 @@ const syncWithImap = async (req, res, next) => {
     );
     dbMails[mailBoxNames[i]] = {};
     userMails.forEach(userMail => {
-      dbMails[mailBoxNames[i]][userMail['message_id']] = userMail;
+      dbMails[mailBoxNames[i]][userMail.message_id] = userMail;
+      delete dbMails[mailBoxNames[i]][userMail.message_id].message_id;
     });
   }
+  return dbMails;
+};
 
+const syncWithImap = async (req, res, next) => {
+  const imapMessageIds = await getImapMessageIds({
+    user: { email: 'yaahoo@daitnu.com', password: '12345678' },
+  });
+  const categories = await DB.Category.findAllByUserNo(4);
+  const dbMails = await getDbMails(imapMessageIds);
   res.send({ imapMessageIds, dbMails, categories });
 };
 
