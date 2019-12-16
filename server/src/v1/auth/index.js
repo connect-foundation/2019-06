f7318418527dc0de25240cac4b4288f4164d3b37
hwanlee <ljhw3377@gmail.com>
2019-12-16 22:30:38 +0900

    fix(be): syncWithImap 함수는 테스트시 제외하도록 함

--- server/src/v1/auth/index.js
+++ server/src/v1/auth/index.js
@@ -7,7 +7,14 @@ import syncWithImap from '../../middlewares/sync-with-imap';
 
 const router = express.Router();
 
-router.post('/login', validateLogin, passport.authenticate('local'), syncWithImap, ctrl.login);
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV === 'test') {
+  router.post('/login', validateLogin, passport.authenticate('local'), ctrl.login);
+} else {
+  router.post('/login', validateLogin, passport.authenticate('local'), syncWithImap, ctrl.login);
+}
+
 router.post('/logout', ctrl.logout);
 
 module.exports = router;
