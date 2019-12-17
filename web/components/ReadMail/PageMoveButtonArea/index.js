b102bd1ed34ae8c77114d6fb7ca743b17f393d09
Jin <dkwk1254@naver.com>
2019-12-17 15:55:45 +0900

    refactor(fe/page-move-button-area): 전역 state.mail 삭제함에 따른 코드 수정
    전역 state.mail을 바꾸는 코드 삭제
    funcToMove() -> actionToMove() 수정
    isLimit() -> isNeedMails() 수정하고 page 조건 추가

--- web/components/ReadMail/PageMoveButtonArea/index.js
+++ web/components/ReadMail/PageMoveButtonArea/index.js
@@ -5,18 +5,18 @@ import { useRouter } from 'next/router';
 import * as S from './styled';
 import request from '../../../utils/request';
 import { AppStateContext, AppDispatchContext } from '../../../contexts';
-import { setMail, handleMailsChange } from '../../../contexts/reducer';
+import { handleMailsChange } from '../../../contexts/reducer';
 import { getQueryByOptions, changeUrlWithoutRunning } from '../../../utils/url/change-query';
 
-const funcToMove = {
+const actionToMove = {
   prev: {
     move: i => i - 1,
-    isLimit: idx => idx === 0,
+    isNeedNewMails: (idx, paging) => idx === 0 && paging.page > 1,
     setIndex: mails => mails.length,
   },
   next: {
     move: i => i + 1,
-    isLimit: (idx, count) => idx === count - 1,
+    isNeedNewMails: (idx, paging, count) => idx === count - 1 && paging.page < paging.totalPage,
     setIndex: () => -1,
   },
 };
@@ -40,30 +40,35 @@ const PageMoveButtonArea = ({ index }) => {
   const { mails, category, sort, paging } = state;
   const mailCount = mails.length;
 
-  const handleMoveBtnClick = async ({ move, isLimit, setIndex }) => {
+  const handleMoveBtnClick = ({ move, isNeedNewMails, setIndex }) => async () => {
     let newMails = mails;
-    if (isLimit(index, mailCount)) {
-      const query = getQueryByOptions({ category, page: move(paging.page), sort });
+    let movedPage = paging.page;
+    if (isNeedNewMails(index, paging, mailCount)) {
+      movedPage = move(paging.page);
+      const query = getQueryByOptions({ category, page: movedPage, sort });
       newMails = await loadNewMails({ query, dispatch });
       index = setIndex(newMails);
     }
     const mail = newMails[move(index)];
-    mail.index = move(index);
-    dispatch(setMail(mail));
-    changeUrlWithoutRunning({ ...urlQuery, mailNo: mail.no });
+    changeUrlWithoutRunning({
+      ...urlQuery,
+      page: movedPage,
+      mailNo: mail.no,
+      mailIndex: mail.index,
+    });
   };
 
   return (
     <S.Container>
       <IconButton
         disabled={isDisabledPrevBtn(index, paging)}
-        onClick={handleMoveBtnClick.bind(null, funcToMove.prev)}>
+        onClick={handleMoveBtnClick(actionToMove.prev)}>
         <ArrowBack />
       </IconButton>
       <S.PageNumberView>{paging.perPageNum * (paging.page - 1) + index + 1}</S.PageNumberView>
       <IconButton
         disabled={isDisabledNextBtn(index, paging, mailCount)}
-        onClick={handleMoveBtnClick.bind(null, funcToMove.next)}>
+        onClick={handleMoveBtnClick(actionToMove.next)}>
         <ArrowForward />
       </IconButton>
     </S.Container>
