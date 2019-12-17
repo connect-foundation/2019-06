631344bf2a047424ac1cf36cbaee460061bd5d9b
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2019-12-17 10:07:34 +0900

    fix(be/storage): 함수명수정 및 간결화 rename대신 renameFile로 명확히 사용한다.

--- server/src/v1/mail/attachment/service.js
+++ server/src/v1/mail/attachment/service.js
@@ -1,7 +1,7 @@
 import DB from '../../../database';
 import ERROR_CODE from '../../../libraries/exception/error-code';
 import ErrorResponse from '../../../libraries/exception/error-response';
-import { download, getStream } from '../../../libraries/storage/ncloud';
+import { downloadFile, getStream } from '../../../libraries/storage/ncloud';
 
 const getAttachment = async ({ attachmentNo, email }) => {
   const mailTemplateAndAttachment = await DB.Attachment.findAttachmentAndMailTemplateByPk(
@@ -21,7 +21,7 @@ const getAttachment = async ({ attachmentNo, email }) => {
     throw new ErrorResponse(ERROR_CODE.PRIVATE_PATH);
   }
 
-  const file = await download(mailTemplateAndAttachment.url);
+  const file = await downloadFile(mailTemplateAndAttachment.url);
   file.mimetype = mailTemplateAndAttachment.type;
   return file;
 };
