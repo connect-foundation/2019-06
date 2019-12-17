e8e3503e89ea1a8bbd286fcf3a9c20afed6317e0
sjh2428 <pok_gare@naver.com>
2019-12-18 00:24:31 +0900

    fix(fe): get dialog부분 bug hotfix

--- web/components/Aside/index.js
+++ web/components/Aside/index.js
@@ -110,8 +110,8 @@ const Aside = () => {
 
   const handleDialogOpen = (action, idx) => {
     const dialogData = getDialogData(
-      handleSnackbarState,
       action,
+      handleSnackbarState,
       state.categories,
       idx + 4,
       setDialogOpen,
